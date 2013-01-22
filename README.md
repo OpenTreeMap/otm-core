d659ba4ad1d55398b483e17c7408c904595e70b0
Adam Hinz <hinz.adam@gmail.com>
2013-01-22 11:52:59 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+OTM IN THE CLOUD
---
+++ opentreemap/opentreemap/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'opentreemap.views.home', name='home'),
+    # url(r'^opentreemap/', include('opentreemap.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+)
