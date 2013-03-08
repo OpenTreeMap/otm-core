dec73d5e72a4d4cec9996b6710bb59d666d91b89
Steve Lamb <slamb@azavea.com>
2013-03-08 18:19:11 -0500

    Add basemap type and basemap data fields
    Added fields to track the type of basemap and the data used to connect to
    this basemap. Also exposed them to the config.

--- opentreemap/treemap/templates/treemap/settings.js
+++ opentreemap/treemap/templates/treemap/settings.js
@@ -6,5 +6,9 @@ otm.settings.instance = {
     'id': '{{ instance.id }}',
     'name': '{{ instance.name }}',
     'rev': '{{ instance.geo_rev_hash }}',
-    'center': [{{ instance.center.x }}, {{ instance.center.y }}]
+    'center': [{{ instance.center.x }}, {{ instance.center.y }}],
+    'basemap': {
+        'type': '{{ instance.basemap_type }}',
+        'data': '{{ instance.basemap_data }}'
+    }
 }
