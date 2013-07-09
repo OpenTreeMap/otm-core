cabc64ca06d4f3909527dd9a44a423c18ac31e14
Adam Hinz <hinz.adam@gmail.com>
2013-07-09 19:50:13 -0400

    Add recent edits route

--- opentreemap/treemap/urls.py
+++ opentreemap/treemap/urls.py
@@ -4,12 +4,14 @@ from __future__ import division
 
 from django.conf.urls import patterns, include, url
 
-from treemap.views import boundary_to_geojson, index, trees, plot_detail, settings_js
+from treemap.views import boundary_to_geojson, index, trees,\
+    plot_detail, settings_js, audits
 
 urlpatterns = patterns(
     '',
     url(r'^$', index),
     url(r'^boundaries/(?P<boundary_id>\d+)/geojson/$', boundary_to_geojson),
+    url(r'^recentedits', audits),
     url(r'^trees/$', trees),
     url(r'^trees/(?P<plot_id>\d+)/$', plot_detail),
     url(r'^config/settings.js$', settings_js)
