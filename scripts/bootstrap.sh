a836821086ecfe0c91f9a2d3e3d4c5bb4da5795c
Michael Maurizi <mmaurizi@azavea.com>
2013-07-10 15:31:39 -0400

    Fix command chaining so umount actually runs

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -14,5 +14,5 @@ fi
 
 # vagrant mounts the share as the wrong user (vagrant)
 # umount and remount as otm.
-mountpoint -q /usr/local/otm/app/ || umount /usr/local/otm/app/
+mountpoint -q /usr/local/otm/app/ && umount /usr/local/otm/app/
 mount -t vboxsf -o uid=`id -u otm`,gid=`id -g vagrant` share /usr/local/otm/app/
