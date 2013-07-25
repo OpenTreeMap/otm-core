fe516e4f6488976b8fb5cafd09c5cea042278dcb
Steve Lamb <slamb@azavea.com>
2013-07-25 14:11:01 -0400

    Add management command to import otm1 data

--- opentreemap/treemap/management/commands/random_trees.py
+++ opentreemap/treemap/management/commands/random_trees.py
@@ -2,25 +2,18 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
 
-from django.core.management.base import BaseCommand
 from django.contrib.gis.geos import Point
+from treemap.models import ImportEvent, Plot, Tree
 from optparse import make_option
-
-from treemap.models import Instance, User, Plot, Tree, ImportEvent,\
-    FieldPermission, Role
+from ._private import InstanceDataCommand
 
 import random
 import math
 
 
-class Command(BaseCommand):
+class Command(InstanceDataCommand):
 
-    option_list = BaseCommand.option_list + (
-        make_option('-i', '--instance',
-                    action='store',
-                    type='int',
-                    dest='instance',
-                    help='Specify the instance to add trees to'),
+    option_list = InstanceDataCommand.option_list + (
         make_option('-r', '--radius',
                     action='store',
                     type='int',
@@ -33,11 +26,6 @@ class Command(BaseCommand):
                     dest='n',
                     default=100000,
                     help='Number of trees to create'),
-        make_option('-d', '--delete',
-                    action='store_true',
-                    dest='delete',
-                    default=False,
-                    help='Delete previous trees/plots in the instance first'),
         make_option('-p', '--prob-of-tree',
                     action='store',
                     type='int',
@@ -48,54 +36,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """ Create some seed data """
-        instance = Instance.objects.get(pk=options['instance'])
-
-        user = User.objects.filter(is_superuser=True)
-
-        if len(user) == 0:
-            print('Error: Could not find a superuser to use')
-            return 1
-        else:
-            user = user[0]
-
-        if user.roles.count() == 0:
-            print('Added global role to user')
-            r = Role(name='global', rep_thresh=0, instance=instance)
-            r.save()
-            user.roles.add(r)
-            user.save()
-
-        for field in ('geom', 'import_event'):
-            _, c = FieldPermission.objects.get_or_create(
-                model_name='Plot',
-                field_name=field,
-                role=user.roles.all()[0],
-                instance=instance,
-                permission_level=FieldPermission.WRITE_DIRECTLY)
-            if c:
-                print('Created plot permission for field "%s"' % field)
-
-        for field in ('plot',):
-            _, c = FieldPermission.objects.get_or_create(
-                model_name='Tree',
-                field_name=field,
-                role=user.roles.all()[0],
-                instance=instance,
-                permission_level=FieldPermission.WRITE_DIRECTLY)
-            if c:
-                print('Created tree permission for field "%s"' % field)
-
-        dt = 0
-        dp = 0
-        if options.get('delete', False):
-            for t in Tree.objects.all():
-                t.delete_with_user(user)
-                dt += 1
-            for p in Plot.objects.all():
-                p.delete_with_user(user)
-                dp += 1
-
-            print("Deleted %s trees and %s plots" % (dt, dp))
+        instance, user = self.setup_env(*args, **options)
 
         n = options['n']
         print("Will create %s plots" % n)
