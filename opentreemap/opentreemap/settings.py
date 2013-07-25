a1af3727eb94b5e95e2c35b30bae83d24ffb2a67
Justin Walgran <jwalgran@azavea.com>
2013-07-25 14:42:56 -0400

    Remove logging settings from settings.py
    The log settings are now configured in a templated local_settings.py.

--- opentreemap/opentreemap/settings.py
+++ opentreemap/opentreemap/settings.py
@@ -138,33 +138,4 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
-}
-
 from opentreemap.local_settings import *  # NOQA
