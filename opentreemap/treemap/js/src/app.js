f0c2cab8333868f6c3e975e5869ae5e672e0033c
Michael Maurizi <mmaurizi@azavea.com>
2013-07-26 13:45:46 -0400

    Modify JS and HTML to use CommonJS style modules

--- opentreemap/treemap/js/src/app.js
+++ opentreemap/treemap/js/src/app.js
@@ -1,164 +1,164 @@
-/* app.js */
+"use strict";
 
-//= require OpenLayers
-//= require openlayers.layer.otm
-//= require Search
+var $ = require('jquery'),
+    OL = require('OpenLayers'),
+    Search = require('./search.js'),
+    Bacon = require('baconjs');
 
-/*globals $,OpenLayers,otm,document,Search*/
-/*jslint indent: 4, white: true */
+require('./openlayers.layer.otm.js');
 
-var app = (function ($,OL,Search,config) {
-    "use strict";
+$.extend($.fn, Bacon.$);
 
-    /* BEGIN BACON HELPERS */
+/* BEGIN BACON HELPERS */
 
-    function keyCodeIs(keyCode) {
-        return function(event) { return event.which === keyCode; };
-    }
+function keyCodeIs(keyCode) {
+    return function(event) { return event.which === keyCode; };
+}
 
-    var isEnterKey = keyCodeIs(13);
+var isEnterKey = keyCodeIs(13);
 
-    /* END BACON HELPERS */
+/* END BACON HELPERS */
 
-    return {
-        createMap: function (elmt) {
-            var map = new OL.Map({
-                theme: null,
-                div: elmt,
-                projection: 'EPSG:3857',
-                layers: this.getBasemapLayers(config)
+var app = {
+    createMap: function (elmt, config) {
+        var map = new OL.Map({
+            theme: null,
+            div: elmt,
+            projection: 'EPSG:3857',
+            layers: this.getBasemapLayers(config)
+        });
+
+        return map;
+    },
+
+    getBasemapLayers: function (config) {
+        var layer;
+        if (config.instance.basemap.type === 'bing') {
+            layer = new OL.Layer.Bing({
+                name: 'Road',
+                key: config.instance.basemap.bing_api_key,
+                type: 'Road',
+                isBaseLayer: true
             });
-
-            return map;
-        },
-
-        getBasemapLayers: function (config) {
-            var layer;
-            if (config.instance.basemap.type === 'bing') {
-                layer = new OL.Layer.Bing({
-                    name: 'Road',
-                    key: config.instance.basemap.bing_api_key,
-                    type: 'Road',
-                    isBaseLayer: true
-                });
-            } else if (config.instance.basemap.type === 'tms') {
-                layer = new OL.Layer.XYZ(
-                    'xyz',
-                    config.instance.basemap.data);
-            } else {
-                layer = new OL.Layer.Google(
-                    "Google Streets",
-                    {numZoomLevels: 20});
-            }
-            return [layer];
-        },
-
-        getPlotLayerURL: function(config, extension) {
-            return '/tile/' +
-                config.instance.rev +
-                '/database/otm/table/treemap_plot/${z}/${x}/${y}.' +
-                extension;
-        },
-
-        createPlotTileLayer: function(config) {
-            return new OL.Layer.OTM(
-                'tiles',
-                this.getPlotLayerURL(config, 'png'),
-            { isBaseLayer: false,
-              filterQueryArgumentName: config.urls.filterQueryArgumentName });
-        },
-
-        getBoundsLayerURL: function(config, extension) {
-            return '/tile/' +
-                config.instance.rev +
-                '/database/otm/table/treemap_boundary/${z}/${x}/${y}.' +
-                extension;
-        },
-
-        createBoundsTileLayer: function(config) {
-            return new OL.Layer.OTM(
-                'bounds',
-                this.getBoundsLayerURL(config, 'png'),
-            { isBaseLayer: false });
-        },
-
-        createPlotUTFLayer: function(config) {
-            return new OL.Layer.UTFGrid({
-                url: this.getPlotLayerURL(config, 'grid.json') +
-                    '?interactivity=id',
-                utfgridResolution: 4
-            });
-        },
-
-        /**
-         * Create a new utf movement control bound to all
-         * utf layers.
-         *
-         * @param renderfn A single argument function
-         *                 that takes in a hash of the last
-         *                 point or 'undefined' if the mouse
-         *                 isn't over a point
-         */
-        createUTFMovementControl: function(renderfn) {
-            return new OL.Control.UTFGrid({
-                callback: function(info) {
-                    var idx, props;
-                    for(idx in info) {
-                        if (info.hasOwnProperty(idx)) {
-                            props = info[idx] || {};
-                            renderfn(props.data);
-                        }
-                    }
-                },
-
-                handlerMode: "move"
-            });
-        },
-
-        onMove: function(data) {
-            document.getElementById("attrs").innerHTML = JSON.stringify(data || {});
-        },
-
-
-        init: function () {
-            var map = app.createMap($("#map")[0]),
-                plotLayer = app.createPlotTileLayer(config),
-                boundsLayer = app.createBoundsTileLayer(config),
-                utfLayer = app.createPlotUTFLayer(config),
-                zoom = 0,
-
-                enterKeyPressStream = $('input[data-class="search"]')
-                    .asEventStream("keyup")
-                    .filter(isEnterKey),
-
-                performSearchClickStream = $("#perform-search")
-                    .asEventStream("click"),
-
-                triggerEventStream = enterKeyPressStream.merge(performSearchClickStream);
-
-            // Bing maps uses a 1-based zoom so XYZ layers
-            // on the base map have a zoom offset that is
-            // always one less than the map zoom:
-            // > map.setCenter(center, 11)
-            // > map.zoom
-            //   12
-            // So this forces the tile requests to use
-            // the correct Z offset
-            if (config.instance.basemap.type === 'bing') {
-                plotLayer.zoomOffset = 1;
-                utfLayer.zoomOffset = 1;
-            }
-
-            map.addLayer(plotLayer);
-            map.addLayer(utfLayer);
-            map.addLayer(boundsLayer);
-
-            map.addControl(app.createUTFMovementControl(app.onMove));
-
-            zoom = map.getZoomForResolution(76.43702827453613);
-            map.setCenter(config.instance.center, zoom);
-
-            Search.init(triggerEventStream, plotLayer);
+        } else if (config.instance.basemap.type === 'tms') {
+            layer = new OL.Layer.XYZ(
+                'xyz',
+                config.instance.basemap.data);
+        } else {
+            layer = new OL.Layer.Google(
+                "Google Streets",
+                {numZoomLevels: 20});
         }
-    };
-}($, OpenLayers, Search, otm.settings));
+        return [layer];
+    },
+
+    getPlotLayerURL: function(config, extension) {
+        return '/tile/' +
+            config.instance.rev +
+            '/database/otm/table/treemap_plot/${z}/${x}/${y}.' +
+            extension;
+    },
+
+    createPlotTileLayer: function(config) {
+        return new OL.Layer.OTM(
+            'tiles',
+            this.getPlotLayerURL(config, 'png'),
+        { isBaseLayer: false,
+          filterQueryArgumentName: config.urls.filterQueryArgumentName });
+    },
+
+    getBoundsLayerURL: function(config, extension) {
+        return '/tile/' +
+            config.instance.rev +
+            '/database/otm/table/treemap_boundary/${z}/${x}/${y}.' +
+            extension;
+    },
+
+    createBoundsTileLayer: function(config) {
+        return new OL.Layer.OTM(
+            'bounds',
+            this.getBoundsLayerURL(config, 'png'),
+        { isBaseLayer: false });
+    },
+
+    createPlotUTFLayer: function(config) {
+        return new OL.Layer.UTFGrid({
+            url: this.getPlotLayerURL(config, 'grid.json') +
+                '?interactivity=id',
+            utfgridResolution: 4
+        });
+    },
+
+    /**
+     * Create a new utf movement control bound to all
+     * utf layers.
+     *
+     * @param renderfn A single argument function
+     *                 that takes in a hash of the last
+     *                 point or 'undefined' if the mouse
+     *                 isn't over a point
+     */
+    createUTFMovementControl: function(renderfn) {
+        return new OL.Control.UTFGrid({
+            callback: function(info) {
+                var idx, props;
+                for(idx in info) {
+                    if (info.hasOwnProperty(idx)) {
+                        props = info[idx] || {};
+                        renderfn(props.data);
+                    }
+                }
+            },
+
+            handlerMode: "move"
+        });
+    },
+
+    onMove: function(data) {
+        document.getElementById("attrs").innerHTML = JSON.stringify(data || {});
+    }
+};
+
+
+module.exports = {
+    init: function (config) {
+        var map = app.createMap($("#map")[0], config),
+            plotLayer = app.createPlotTileLayer(config),
+            boundsLayer = app.createBoundsTileLayer(config),
+            utfLayer = app.createPlotUTFLayer(config),
+            zoom = 0,
+
+            enterKeyPressStream = $('input[data-class="search"]')
+                .asEventStream("keyup")
+                .filter(isEnterKey),
+
+            performSearchClickStream = $("#perform-search")
+                .asEventStream("click"),
+
+            triggerEventStream = enterKeyPressStream.merge(performSearchClickStream);
+
+        // Bing maps uses a 1-based zoom so XYZ layers
+        // on the base map have a zoom offset that is
+        // always one less than the map zoom:
+        // > map.setCenter(center, 11)
+        // > map.zoom
+        //   12
+        // So this forces the tile requests to use
+        // the correct Z offset
+        if (config.instance.basemap.type === 'bing') {
+            plotLayer.zoomOffset = 1;
+            utfLayer.zoomOffset = 1;
+        }
+
+        map.addLayer(plotLayer);
+        map.addLayer(utfLayer);
+        map.addLayer(boundsLayer);
+
+        map.addControl(app.createUTFMovementControl(app.onMove));
+
+        zoom = map.getZoomForResolution(76.43702827453613);
+        map.setCenter(config.instance.center, zoom);
+
+        Search.init(triggerEventStream, plotLayer, config);
+    }
+};
--- opentreemap/treemap/js/src/search.js
+++ opentreemap/treemap/js/src/search.js
@@ -1,78 +1,71 @@
-/* search.js */
+"use strict";
 
-//= require Bacon
-//= require underscore
+var $ = require('jquery'),
+    Bacon = require('baconjs'),
+    _ = require("underscore");
 
-/*jslint nomen: true*/
-/*globals $,_,OpenLayers,otm,document,Bacon,JSON*/
-/*jslint indent: 4, white: true */
+var config,
+    // Quite manual right now but should be nicer in the future
+    elems = { '#search-species':
+              { 'key': 'species.id',
+                'pred': 'IS' },
+              '#dbh-min':
+              { 'key': 'tree.diameter',
+                'pred': 'MIN' },
+              '#boundary':
+              { 'key': 'plot.geom',
+                'pred': 'IN_BOUNDARY' },
+              '#dbh-max':
+              { 'key': 'tree.diameter',
+                'pred': 'MAX' }};
 
-var Search = (function ($,Bacon,config) {
-    "use strict";
+function buildSearch(stream) {
+    return _.reduce(elems, function(preds, key_and_pred, id) {
+        var val = $(id).val(),
+            pred = {};
 
-    var exports = {},
-        // Quite manual right now but should be nicer in the future
-        elems = { '#search-species':
-                  { 'key': 'species.id',
-                    'pred': 'IS' },
-                  '#dbh-min':
-                  { 'key': 'tree.diameter',
-                    'pred': 'MIN' },
-                  '#boundary':
-                  { 'key': 'plot.geom',
-                    'pred': 'IN_BOUNDARY' },
-                  '#dbh-max':
-                  { 'key': 'tree.diameter',
-                    'pred': 'MAX' }};
+        if (val && val.length > 0) {
+            pred[key_and_pred.pred] = val;
+            preds[key_and_pred.key] = pred;
+        }
 
-    function buildSearch(stream) {
-        return _.reduce(elems, function(preds, key_and_pred, id) {
-            var val = $(id).val(),
-                pred = {};
+        return preds;
+    }, {});
+}
 
-            if (val && val.length > 0) {
-                pred[key_and_pred.pred] = val;
-                preds[key_and_pred.key] = pred;
-            }
+function executeSearch(search_query) {
+    var search = $.ajax({
+        url: '/' + config.instance.id + '/benefit/search',
+        data: {'q': JSON.stringify(search_query)},
+        type: 'GET',
+        dataType: 'html'});
 
-            return preds;
-        }, {});
-    }
+    return Bacon.fromPromise(search);
+}
 
-    function executeSearch(search_query) {
-        var search = $.ajax({
-            url: '/' + config.instance.id + '/benefit/search',
-            data: {'q': JSON.stringify(search_query)},
-            type: 'GET',
-            dataType: 'html'});
+function displaySearch(result) {
+    $("#search-results").html(result);
+}
 
-        return Bacon.fromPromise(search);
-    }
+// Arguments
+//
+// triggerEventStream: a Bacon.js EventStream. The value
+//   of the item will be ignored and, instead, the current
+//   values of the search form fields will be scraped.
+//
+// plotLayer: An OpenLayers.Layer.OTM instance with the
+// `url` argument initialized to the unfiltered tile url.
+//
+// otmConfig: The otm.settings config object
+exports.init = function(triggerEventStream, plotLayer, otmConfig) {
+    config = otmConfig;
+    var searchStream = triggerEventStream.map(buildSearch);
 
-    function displaySearch(result) {
-        $("#search-results").html(result);
-    }
+    // binding is required to save the context of the plotLayer
+    // `setFilter` instance method
+    searchStream.onValue(_.bind(plotLayer.setFilter, plotLayer));
 
-    // Arguments
-    //
-    // triggerEventStream: a Bacon.js EventStream. The value
-    //   of the item will be ignored and, instead, the current
-    //   values of the search form fields will be scraped.
-    //
-    // plotLayer: An OpenLayers.Layer.OTM instance with the
-    // `url` argument initialized to the unfiltered tile url.
-    exports.init = function(triggerEventStream, plotLayer) {
-        var searchStream = triggerEventStream.map(buildSearch);
-
-        // binding is required to save the context of the plotLayer
-        // `setFilter` instance method
-        searchStream.onValue(_.bind(plotLayer.setFilter, plotLayer));
-
-        searchStream
-            .flatMap(executeSearch)
-            .onValue(displaySearch);
-    };
-
-    return exports;
-
-}($, Bacon, otm.settings));
+    searchStream
+        .flatMap(executeSearch)
+        .onValue(displaySearch);
+};
