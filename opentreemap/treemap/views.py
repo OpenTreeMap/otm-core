264039f493e0e0ad093ad980ca55c81d37ddef12
Adam Hinz <hinz.adam@gmail.com>
2013-07-30 10:28:39 -0400

    A gift to whoever broke the build with PEP8 errors...

--- opentreemap/treemap/views.py
+++ opentreemap/treemap/views.py
@@ -310,8 +310,9 @@ plot_detail_view = instance_request(etag(_plot_hash)(
     render_template('treemap/plot_detail.html', plot_detail)))
 
 root_settings_js_view = render_template('treemap/settings.js',
-                    {'BING_API_KEY': settings.BING_API_KEY},
-                    mimetype='application/javascript')
+                                        {'BING_API_KEY':
+                                         settings.BING_API_KEY},
+                                        mimetype='application/javascript')
 
 instance_settings_js_view = instance_request(
     render_template('treemap/settings.js',
