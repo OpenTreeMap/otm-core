657ecc493088e21fc7fd33a6d8273d860ce1b119
Adam Hinz <hinz.adam@gmail.com>
2013-09-03 13:34:48 -0400

    Add in selenium test driver
    Run uitests with:
    
    ```
    python manage.py uitest
    ```
    
    This commit just gets everything set up and moving. Future work includes
    support for filtering tests on the command line (similar to manage.py
    test).
    
    A 'dev-requirements.txt' file that include requirements for running
    selenium has also been added. To run the tests you'll need firefox and
    some sort of X server.

---
+++ opentreemap/treemap/management/commands/uitest.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+
+import unittest
+import sys
+
+from django.core.management.base import BaseCommand
+from django.conf import settings
+
+from pyvirtualdisplay import Display
+
+import uitests
+
+
+class Command(BaseCommand):
+    """
+    Uses a custom test runner to run UI acceptance tests
+    from the 'tests' package
+    """
+
+    def handle(self, *args, **options):
+        if settings.DEBUG is False:
+            raise Exception('These tests add data to the currently '
+                            'select database backend. If this is a '
+                            'production database a failing test could '
+                            'leave extra data behind (such as users) or '
+                            'delete data that already exists.')
+
+        suite = unittest.TestLoader().loadTestsFromModule(uitests)
+
+        disp = Display(visible=0, size=(800, 600))
+        disp.start()
+
+        try:
+            uitests.setUpModule()
+            rslt = unittest.TextTestRunner(verbosity=2).run(suite)
+        finally:
+            uitests.tearDownModule()
+            disp.stop()
+
+        if rslt.failures:
+            sys.exit(1)
