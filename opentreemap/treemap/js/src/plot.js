ec7d74c1eb39bab1be5dc265b771bf5f8cbe6bf5
Rick Mohr <rmohr@azavea.com>
2013-09-06 12:57:46 -0400

    Create reusable plotMover.js to use on both map page and plot detail page

--- opentreemap/treemap/js/src/plot.js
+++ opentreemap/treemap/js/src/plot.js
@@ -5,6 +5,7 @@ var $ = require('jquery'),
     otmTypeahead = require('./otmTypeahead'),
     inlineEditForm = require('./inlineEditForm'),
     mapManager = require('./mapManager'),
+    plotMover = require('./plotMover'),
     plotMarker = require('./plotMarker');
 
 // For modal dialog on jquery
@@ -33,62 +34,25 @@ exports.init = function(options) {
     inlineEditForm.init(
         _.extend(options.inlineEditForm, { onSaveBefore: onSaveBefore }))
 
-    var $editLocationButton = $(options.plotLocation.edit),
-        $cancelEditLocationButton = $(options.plotLocation.cancel),
-        location = options.plotLocation.location,
-        map = mapManager.init({
-            config: options.config,
-            selector: '#map',
-            center: location,
-            zoom: mapManager.ZOOM_PLOT
-        });
-
-    plotMarker.init(map);
-    plotMarker.place(location);
-
-    inlineEditForm.inEditModeProperty.onValue(function (inEditMode) {
-        // Form is changing to edit mode or display mode
-        if (inEditMode) {
-            $editLocationButton.show();
-        } else { // in display mode
-            $editLocationButton.hide();
-            plotMarker.disableMoving();
-        }
-        $cancelEditLocationButton.hide();
+    mapManager.init({
+        config: options.config,
+        selector: '#map',
+        center: options.plotLocation.location,
+        zoom: mapManager.ZOOM_PLOT
     });
 
-    inlineEditForm.cancelStream.onValue(function () {
-        // User clicked the form's "Cancel" button. Restore plot location.
-        plotMarker.place(location);
-    });
+    plotMarker.init(mapManager.map);
 
-    $editLocationButton.click(function () {
-        // User clicked the "Move Tree" button
-        $editLocationButton.hide();
-        $cancelEditLocationButton.show();
-        plotMarker.enableMoving();
-    });
-
-    $cancelEditLocationButton.click(function () {
-        // User clicked the "Cancel Tree Move" button
-        $editLocationButton.show();
-        $cancelEditLocationButton.hide();
-        plotMarker.disableMoving();
-        plotMarker.place(location);  // Restore plot location
-    });
+    plotMover.init({
+        mapManager: mapManager,
+        plotMarker: plotMarker,
+        inlineEditForm: inlineEditForm,
+        editLocationButton: options.plotLocation.edit,
+        cancelEditLocationButton: options.plotLocation.cancel,
+        location: options.plotLocation.location
+    })
 
     function onSaveBefore(data) {
-        // Form is about to save its data
-        if (plotMarker.wasMoved()) {
-            // Add plot location to data object
-            data['plot.geom'] = plotMarker.getLocation();
-        }
+        plotMover.onSaveBefore(data);
     }
-
-    inlineEditForm.saveOkStream.onValue(function (result) {
-        // Form successfully saved its data. Update cached plot location.
-        location = plotMarker.getLocation();
-        // Refresh the map if appropriate
-        mapManager.updateGeoRevHash(result.geoRevHash);
-    });
 };
