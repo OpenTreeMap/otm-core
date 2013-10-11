OpenTreeMap/otm-core

commit bb8a9a9d618264508ae921a7281691f66dbcc37f

Don't serialize unchanged fields.

opentreemap/treemap/js/src/fieldHelpers.js
@@ -8,22 +8,44 @@ var $ = require('jquery'),
 var getField = exports.getField = function ($fields, name) {
     return $fields.filter('[data-field="' + name + '"]');
 };
-
-exports.getSerializableField = function ($fields, name) {
+var getSerializableField = exports.getSerializableField = function ($fields, name) {
     // takes a jQuery collection of edit fields and returns the
     // actual input or select field that will be serialized
     return getField($fields, name).find('[name="' + name + '"]');
 };
 
-exports.formToDictionary = function ($form, $editFields) {
+exports.formToDictionary = function ($form, $editFields, $displayFields) {
+    $displayFields = $displayFields || $();
+
     var isTypeaheadHiddenField = function(name) {
-        return $form.find('[name="' + name + '"]').is('[data-typeahead-hidden]');
+        return getSerializableField($editFields, name).is('[data-typeahead-hidden]');
     };
+    var getDisplayValue = function(type, name) {
+        var $displayField;
+        if (isTypeaheadHiddenField(name)) {
+            return $form.find('[data-typeahead-restore="' + name + '"]').val();
+        }
+        $displayField = getField($displayFields, name);
+        if ($displayField.is('[data-value]')) {
+            if (type === 'bool') {
+                return $displayField.attr('data-value') === "True";
+            }
+            return $displayField.attr('data-value');
+        }
+        return undefined;
+    };
+
     var result = {};
     _.each($form.serializeArray(), function(item) {
-        var type = exports.getField($editFields, item.name).data('type');
-        if (type === 'bool'){
-            return;
+        var type = getField($editFields, item.name).attr('data-type'),
+            displayValue = getDisplayValue(type, item.name);
+
+        if (item.value === displayValue) {
+            return;  // Don't serialize unchanged values
+        }
+
+        if (type === 'bool') {
+            result[item.name] = getSerializableField($editFields, item.name).is(":checked");
         } else if (item.value === '' && (type === 'int' || type === 'float')) {
             // convert empty numeric fields to null
             result[item.name] = null;
@@ -33,9 +55,6 @@ exports.formToDictionary = function ($form, $editFields) {
         } else {
             result[item.name] = item.value;
         }
-    });
-    $form.find('input[name][type="checkbox"]').each(function() {
-        result[this.name] = this.checked;
     });
     return result;
 };

opentreemap/treemap/js/src/inlineEditForm.js
@@ -140,7 +140,7 @@ exports.init = function(options) {
         },
 
         getDataToSave = function() {
-            var data = FH.formToDictionary($(form), $(editFields));
+            var data = FH.formToDictionary($(form), $(editFields), $(displayFields));
 
             // Extract data for all rows of the collection,
             // whether entered in this session or pre-existing.

commit c557485306bc96c668fd3c0e36ec830bf3576810

Fix regression introduced by bb8a9a9, always serialize unchecked boxes

opentreemap/treemap/js/src/fieldHelpers.js
@@ -45,7 +45,8 @@ exports.formToDictionary = function ($form, $editFields, $displayFields) {
         }
 
         if (type === 'bool') {
-            result[item.name] = getSerializableField($editFields, item.name).is(":checked");
+            // Handled below so we catch unchecked checkboxes which
+            // serializeArray ignores
         } else if (item.value === '' && (type === 'int' || type === 'float')) {
             // convert empty numeric fields to null
             result[item.name] = null;
@@ -54,6 +55,11 @@ exports.formToDictionary = function ($form, $editFields, $displayFields) {
             result[item.name] = null;
         } else {
             result[item.name] = item.value;
+        }
+    });
+    $form.find('[name][type="checkbox"]').not('[disabled]').each(function(i, elem) {
+        if (elem.checked !== getDisplayValue('bool', elem.name)) {
+            result[elem.name] = elem.checked;
         }
     });
     return result;