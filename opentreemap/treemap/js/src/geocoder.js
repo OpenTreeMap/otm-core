c22e8ee7ccfe17d045a169d1bc0517926062e001
Justin Walgran <jwalgran@azavea.com>
2013-10-15 18:01:19 -0400

    Add reverseGeocoding to geocoder.js
    Allows the add tree client code to lookup the address for a new point.

--- opentreemap/treemap/js/src/geocoder.js
+++ opentreemap/treemap/js/src/geocoder.js
@@ -5,6 +5,33 @@ var $ = require('jquery'),
     _ = require('underscore'),
     Bacon = require('baconjs');
 
+// ``coordToLatLng`` converts a 2d coordinate array or an object
+// to an object with ``lat`` and ``lng`` values.
+function coordToLatLngObject(latLng) {
+    var lat, lng;
+    if (_.isArray(latLng)) {
+        lat = latLng[0];
+        lng = latLng[1];
+    } else {
+        if (latLng.lat) { lat = latLng.lat; }
+        if (latLng.lng) { lng = latLng.lng; }
+        if (latLng.lon) { lng = latLng.lon; }
+        if (latLng.x) { lng = latLng.x; }
+        if (latLng.y) { lat = latLng.y; }
+    }
+    return {
+        lat: lat,
+        lng: lng
+    };
+}
+
+// ``latLngToParam`` converts an object with ``lat`` and ``lng``
+// values to a string suitable to be passed as a ``location`` query
+// string argument to the ESRI reverse geocoder REST service
+function latLngToParam(latLng) {
+    return latLng.lng + ', ' + latLng.lat;
+}
+
 exports = module.exports = function (config) {
 
     var geocodeServer = function (address, bbox, success, error) {
@@ -108,6 +135,26 @@ exports = module.exports = function (config) {
             .map(filterBbox, box);
     };
 
+    var reverseGeocodeClient = function(latLng, distance) {
+        var url = 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/reverseGeocode';
+        var params = {
+            'location': latLngToParam(latLng),
+            'distance': distance,
+            'outSR': '3857', // Returning web mercator given latlng is a helpful side-effect
+            'f': 'json'
+        };
+
+        return Bacon.fromPromise(
+            $.ajax({
+                url: url,
+                type: 'GET',
+                data: params,
+                crossDomain: true,
+                dataType: 'jsonp'
+            }));
+    };
+
+
     return {
         geocodeStream: function(addressStream) {
             return addressStream.flatMap(function (address) {
@@ -118,6 +165,19 @@ exports = module.exports = function (config) {
                 } else {
                     return Bacon.once(
                         new Bacon.Error(config.geocoder.errorString));
+                }
+            });
+        },
+
+        reverseGeocodeStream: function(coordStream) {
+            return coordStream.map(coordToLatLngObject).flatMap(function (latLng) {
+                return reverseGeocodeClient(latLng, config.reverseGeocodeDistance || 200);
+            }).flatMap(function (response) {
+                if (response && response.address) {
+                    return Bacon.once(response);
+                } else {
+                    return Bacon.once(new Bacon.Error(
+                        config.geocoder.reverseGeocoderErrorString));   
                 }
             });
         }
