OpenTreeMap/otm-core

commit 91b7ad991d49bf4f46a28c597099767c90282809

Split validation errors stream out of response stream.
Fixes an issue where on a generic error the validation error elements
were being shown despite there being no messages to display.

opentreemap/treemap/js/src/inlineEditForm.js
@@ -205,7 +205,10 @@ exports.init = function(options) {
 
         responseStream = saveStream
             .map(getDataToSave)
-            .flatMap(update)
+            .flatMap(update),
+
+        responseErrorStream = responseStream
+            .errors()
             .mapError(function (e) {
                 var result = ('responseJSON' in e) ? e.responseJSON : {};
                 if (!('error' in result)) {
@@ -216,55 +219,55 @@ exports.init = function(options) {
                 return result;
             }),
 
-        saveOkStream = responseStream.filter(function (result) {
-            return !('error' in result);
-        }).map(function(responseData) {
+        saveOkStream = responseStream.map(function(responseData) {
             return {
                 formData: getDataToSave(),
                 responseData: responseData
             };
         }),
 
-        hideAndShowElements = function (action) {
-            function hideOrShow(fields, actions) {
-                if (_.contains(actions, action)) {
-                    $(fields).show();
+        hideAndShowElements = function (fields, actions, action) {
+            if (_.contains(actions, action)) {
+                $(fields).show();
+            } else {
+                if (action === 'edit:start') {
+                    // always hide the applicable runmode buttons
+                    $(fields).filter('.btn').hide();
+
+                    // hide the display fields if there is a corresponding
+                    // edit field to show in its place
+                    _.each($(fields).filter(":not(.btn)"), function (field) {
+                        var $field = $(field),
+                            $edit = FH.getField($(editFields),
+                                                $field.attr('data-field'));
+
+                        if ($edit.length === 1) {
+                            $field.hide();
+                        }
+
+                    });
+
                 } else {
-                    if (action === 'edit:start') {
-                        // always hide the applicable runmode buttons
-                        $(fields).filter('.btn').hide();
-
-                        // hide the display fields if there is a corresponding
-                        // edit field to show in its place
-                        _.each(FH.excludeButtons(fields), function (field) {
-                            var $field = $(field),
-                                $edit = FH.getField($(editFields),
-                                                    $field.attr('data-field'));
-
-                            if ($edit.length === 1) {
-                                $field.hide();
-                            }
-
-                        });
-
-                    } else {
-                        $(fields).hide();
-                    }
+                    $(fields).hide();
                 }
             }
+        },
 
-            hideOrShow(editFields, eventsLandingInEditMode);
-            hideOrShow(displayFields, eventsLandingInDisplayMode);
-            hideOrShow(validationFields, ['save:error']);
-        };
+        enableOrDisableEditButton = function () {
+            var disable = $(editFields).filter(':not(.btn)').length === 0;
+            $edit.prop('disabled', disable);
+            $edit.attr('title', disable ? disabledMessage : '');
+        },
+
+        validationErrorsStream = responseErrorStream
+            .filter('.validationErrors')
+            .map('.validationErrors');
 
     saveOkStream
         .map('.formData')
         .onValue(formFieldsToDisplayValues);
 
-    responseStream.filter('.validationErrors')
-                  .map('.validationErrors')
-                  .onValue(showValidationErrorsInline);
+    validationErrorsStream.onValue(showValidationErrorsInline);
 
     // TODO: Show success toast
     // TODO: Show error toast
@@ -283,7 +286,7 @@ exports.init = function(options) {
     }
 
     actionStream.plug(
-        responseStream.filter('.error').map('save:error')
+        responseErrorStream.map('save:error')
     );
 
     actionStream.plug(
@@ -298,7 +301,11 @@ exports.init = function(options) {
             .filter(_.contains, eventsLandingInDisplayMode)
             .onValue(resetCollectionUdfs);
 
-    actionStream.onValue(hideAndShowElements);
+    actionStream.onValue(hideAndShowElements, editFields, eventsLandingInEditMode);
+    actionStream.onValue(hideAndShowElements, displayFields, eventsLandingInDisplayMode);
+    validationErrorsStream
+        .map('save:error')
+        .onValue(hideAndShowElements, validationFields, ['save:error']);
 
     var inEditModeProperty = actionStream.map(function (event) {
         return _.contains(eventsLandingInEditMode, event);

commit 43c12d237e6e03adce1df031d5c6fdd28bd8bb0d

Only show inline edit fields when they have content to show, Fixes #706 Fixes issue with validation errors showing for fields with no errors
A regression was introduced in 91b7ad99 where validation errors were being
shown when there were validation errors, but then never hidden.

This is now changed to always show/hide validation errors for all events in the
actionStream, but to only show fields which have something in them.

opentreemap/treemap/js/src/inlineEditForm.js
@@ -234,7 +234,12 @@ exports.init = function(options) {
 
         hideAndShowElements = function (fields, actions, action) {
             if (_.contains(actions, action)) {
-                $(fields).show();
+                $(fields).each(function() {
+                    var $field = $(this);
+                    if ($field.html().trim() !== "") {
+                        $field.show();
+                    }
+                });
             } else {
                 if (action === 'edit:start') {
                     // always hide the applicable runmode buttons
@@ -317,9 +322,7 @@ exports.init = function(options) {
 
     actionStream.onValue(hideAndShowElements, editFields, eventsLandingInEditMode);
     actionStream.onValue(hideAndShowElements, displayFields, eventsLandingInDisplayMode);
-    validationErrorsStream
-        .map('save:error')
-        .onValue(hideAndShowElements, validationFields, ['save:error']);
+    actionStream.onValue(hideAndShowElements, validationFields, ['save:error']);
 
     var inEditModeProperty = actionStream.map(function (event) {
         return _.contains(eventsLandingInEditMode, event);