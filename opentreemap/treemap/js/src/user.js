859af8a8ae2429e98f7d41ec67ae3cddb4a55655
Michael Maurizi <mmaurizi@azavea.com>
2013-10-31 21:00:14 -0400

    Add photo upload to user profile page

--- opentreemap/treemap/js/src/user.js
+++ opentreemap/treemap/js/src/user.js
@@ -1,9 +1,17 @@
 "use strict";
 
-var inlineEditForm = require('treemap/inlineEditForm');
-var recentEdits = require('treemap/recentEdits');
+var inlineEditForm = require('treemap/inlineEditForm'),
+    recentEdits = require('treemap/recentEdits'),
+    imageUploadPanel = require('treemap/imageUploadPanel'),
+    csrf = require('treemap/csrf'),
+    $ = require('jquery');
 
 exports.init = function(options) {
+    $.ajaxSetup(csrf.jqueryAjaxSetupOptions);
+
     inlineEditForm.init(options.inlineEditForm);
     recentEdits.init(options.recentEdits);
+    if (options.imageUploadPanel) {
+        imageUploadPanel.init(options.imageUploadPanel);
+    }
 };
