OpenTreeMap/otm-core

commit 5c7162509fcf4d63971fb9090c154fc669b2453a

Loosen up identifier regex, allowing variables not named after models
Whitelisting the model part of model.field was a bit of a misnomer, since
it whitelisted the template variable name, not the actual model class it
represented.

Identifiers still need to be of the form "model.field" though, in order to
access the field metadata on the model class

opentreemap/treemap/templatetags/form_extras.py
@@ -21,12 +21,11 @@ from treemap.units import (get_digits, get_units, is_formattable,
 
 register = template.Library()
 
-# Used to whitelist the model.field values that are valid for the
-# template tag, can't be done in the grammar as it can't be checked
+# Used to check that the identifier follows the format model.field or
+# model.udf:field name, can't be done in the grammar as it can't be checked
 # until looked up in the context
 _identifier_regex = re.compile(
-    r"^(?:tree|plot|instance|user|species|"
-    "benefitCurrencyConversion)\.(?:udf\:)?[\w '\._-]+$")
+    r"^[a-zA-Z_]+\.(?:udf\:[\w '\._-]+|[a-zA-Z_]+)$")
 
 
 class Variable(Grammar):

commit 00373d19d193560823850cbbfbfa2510f3e45eed

Fix regex error breaking management page
Introduced in 5c7162509fc

The change prevented nested lookups:
instance.config.linkData

And dashes:
instance.scss-colop

opentreemap/treemap/templatetags/form_extras.py
@@ -25,7 +25,7 @@ register = template.Library()
 # model.udf:field name, can't be done in the grammar as it can't be checked
 # until looked up in the context
 _identifier_regex = re.compile(
-    r"^[a-zA-Z_]+\.(?:udf\:[\w '\._-]+|[a-zA-Z_]+)$")
+    r"^[a-zA-Z_.\-]+(?:udf\:[\w '\._-]+|[a-zA-Z_\-]+)$")
 
 
 class Variable(Grammar):