OpenTreeMap/otm-core

commit a46c0579e8a4598a0ad96901cfa7f3b8275f31e4

Order CSV columns
fixes #1054 on github

opentreemap/exporter/tasks.py
@@ -88,11 +88,13 @@ def csv_export(job_pk, model, query):
             extra_select.update(extra_select_plot)
             extra_select.update(extra_select_sp)
 
-            values = values_tree + values_plot + values_sp
+            ordered_fields = (values_tree + extra_select_tree.keys() +
+                              sorted(values_plot + extra_select_plot.keys()) +
+                              sorted(values_sp + extra_select_sp.keys()))
 
-            if values:
+            if ordered_fields:
                 limited_qs = initial_qs.extra(select=extra_select)\
-                                       .values(*values)
+                                       .values(*ordered_fields)
             else:
                 limited_qs = initial_qs.none()
 
@@ -109,7 +111,8 @@ def csv_export(job_pk, model, query):
         job.status = ExportJob.MODEL_PERMISSION_ERROR
     else:
         csv_file = TemporaryFile()
-        write_csv(limited_qs, csv_file)
+
+        write_csv(limited_qs, csv_file, field_order=ordered_fields)
 
         csv_name = generate_filename(limited_qs)
         job.outfile.save(csv_name, File(csv_file))

commit 0b0b4e95dfa33709ca4336711a8e42f38d430466

Remove duplicate UDF columns in CSV exports
This regression was introduced by a46c0579e8a4598a0ad96901cfa7f3b8275f31e4.

the extra_select fields are _already_ included in the values list. Stop including them twice.

opentreemap/exporter/tasks.py
@@ -90,9 +90,9 @@ def csv_export(job_pk, model, query):
             extra_select.update(extra_select_plot)
             extra_select.update(extra_select_sp)
 
-            ordered_fields = (values_tree + extra_select_tree.keys() +
-                              sorted(values_plot + extra_select_plot.keys()) +
-                              sorted(values_sp + extra_select_sp.keys()))
+            ordered_fields = (sorted(values_tree) +
+                              sorted(values_plot) +
+                              sorted(values_sp))
 
             if ordered_fields:
                 limited_qs = initial_qs.extra(select=extra_select)\