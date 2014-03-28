287b895dbf6458c0d0de8c4e65d5ee35293b6bf2
Adam Hinz <hinz.adam@gmail.com>
2014-03-28 10:57:13 -0400

    Resource -> Watershed Solution

--- opentreemap/treemap/js/src/addResourceMode.js
+++ opentreemap/treemap/js/src/addResourceMode.js
@@ -60,7 +60,7 @@ function init(options) {
             manager.stepControls.enableNext(STEP_OUTLINE_AREA, true);
             manager.stepControls.enableNext(STEP_DETAILS, false);
             $summaryHead.text(typeName);
-            $summarySubhead.text("Resource");
+            $summarySubhead.text("Watershed Solution");
 
             var hasAreaStep = !!areaFieldName;
             activateAreaStep(hasAreaStep);
