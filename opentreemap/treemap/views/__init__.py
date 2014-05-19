ebe91e1fb5ae6691fb44b9010ee7f0add71ebcf6
Steve Lamb <slamb@azavea.com>
2014-05-19 10:45:20 -0400

    Cleanup decorators
    Using currying and higher order plumbing to make the process cleaner and easier to reason about.

--- opentreemap/treemap/views/__init__.py
+++ opentreemap/treemap/views/__init__.py
@@ -7,10 +7,10 @@ from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import etag
 
-from opentreemap.util import route
+from opentreemap.util import route, decorate as do
 
 from treemap.decorators import (json_api_call, render_template, login_or_401,
-                                require_http_method, string_as_file_call,
+                                require_http_method, string_to_response,
                                 requires_feature,
                                 creates_instance_user, instance_request,
                                 username_matches_request_user,
@@ -49,40 +49,37 @@ from treemap.views.map_feature import (render_map_feature_detail,
 
 edits_view = instance_request(
     requires_feature('recent_edits_report')(
-        render_template('treemap/edits.html', edits)))
+        render_template('treemap/edits.html')(edits)))
 
 index_view = instance_request(index)
 
 map_view = instance_request(
-    render_template('treemap/map.html', get_map_view_context))
+    render_template('treemap/map.html')(get_map_view_context))
 
 static_page_view = instance_request(
-    render_template("treemap/staticpage.html", static_page))
+    render_template('treemap/staticpage.html')(static_page))
 
 instance_not_available_view = render_template(
-    "treemap/instance_not_available.html")
+    'treemap/instance_not_available.html')()
 
-landing_view = render_template("base.html")
+landing_view = render_template('base.html')()
 
-unsupported_view = render_template("treemap/unsupported.html")
+unsupported_view = render_template('treemap/unsupported.html')()
 
-error_404_view = render_template('404.html', statuscode=404)
-error_500_view = render_template('500.html', statuscode=500)
-error_503_view = render_template('503.html', statuscode=503)
+error_404_view = render_template('404.html')(statuscode=404)
+error_500_view = render_template('500.html')(statuscode=500)
+error_503_view = render_template('503.html')(statuscode=503)
 
 #####################################
 # utility views
 #####################################
 
-root_settings_js_view = render_template('treemap/settings.js',
-                                        {'BING_API_KEY':
-                                         settings.BING_API_KEY},
-                                        mimetype='application/javascript')
+root_settings_js_view = render_template('treemap/settings.js')(
+    {'BING_API_KEY':
+     settings.BING_API_KEY},
+    mimetype='application/javascript')
 
-instance_settings_js_view = instance_request(
-    render_template('treemap/settings.js',
-                    {'BING_API_KEY': settings.BING_API_KEY},
-                    mimetype='application/javascript'))
+instance_settings_js_view = instance_request(root_settings_js_view)
 
 boundary_to_geojson_view = json_api_call(instance_request(boundary_to_geojson))
 boundary_autocomplete_view = instance_request(
@@ -90,8 +87,10 @@ boundary_autocomplete_view = instance_request(
 
 species_list_view = json_api_call(instance_request(species_list))
 
-scss_view = require_http_method("GET")(
-    string_as_file_call("text/css", compile_scss))
+scss_view = do(
+    require_http_method("GET"),
+    string_to_response("text/css"),
+    compile_scss)
 
 #####################################
 # mapfeature
@@ -140,13 +139,13 @@ add_map_feature_photo_endpoint = require_http_method("POST")(
 edit_plot_detail_view = login_required(
     instance_request(
         creates_instance_user(
-            render_template('treemap/plot_detail.html', plot_detail))))
+            render_template('treemap/plot_detail.html')(plot_detail))))
 
 get_plot_eco_view = instance_request(etag(map_feature_hash)(
-    render_template('treemap/partials/plot_eco.html', plot_detail)))
 
 plot_accordion_view = instance_request(
     render_template('treemap/plot_accordion.html', plot_detail))
+    render_template('treemap/partials/plot_eco.html')(plot_detail)))
 
 
 #####################################
@@ -160,23 +159,25 @@ delete_tree_view = login_or_401(
 
 tree_detail_view = instance_request(tree_detail)
 
-search_tree_benefits_view = instance_request(
-    etag(search_hash)(
-        render_template('treemap/partials/eco_benefits.html',
-                        search_tree_benefits)))
+search_tree_benefits_view = do(
+    instance_request,
+    etag(search_hash),
+    render_template('treemap/partials/eco_benefits.html'),
+    search_tree_benefits)
 
-add_tree_photo_endpoint = require_http_method("POST")(
-    login_or_401(
-        instance_request(
-            creates_instance_user(
-                render_template("treemap/partials/tree_carousel.html",
-                                add_tree_photo)))))
+add_tree_photo_endpoint = do(
+    require_http_method("POST"),
+    login_or_401,
+    instance_request,
+    creates_instance_user,
+    render_template('treemap/partials/tree_carousel.html'),
+    add_tree_photo)
 
 #####################################
 # user
 #####################################
 
-user_view = render_template("treemap/user.html", user)
+user_view = render_template('treemap/user.html')(user)
 
 instance_user_view = instance_user
 
@@ -188,16 +189,16 @@ update_user_view = require_http_method("PUT")(
     username_matches_request_user(
         json_api_call(update_user)))
 
-user_audits_view = render_template("treemap/recent_user_edits.html",
-                                   user_audits)
+user_audits_view = render_template(
+    'treemap/recent_user_edits.html')(user_audits)
 
 upload_user_photo_view = require_http_method("POST")(
     username_matches_request_user(
         json_api_call(upload_user_photo)))
 
 forgot_username_view = route(
-    GET=render_template('treemap/forgot_username.html'),
-    POST=render_template('treemap/forgot_username_done.html', forgot_username))
+    GET=render_template('treemap/forgot_username.html')(),
+    POST=render_template('treemap/forgot_username_done.html')(forgot_username))
 
 #####################################
 # photo
@@ -206,18 +207,20 @@ forgot_username_view = route(
 
 photo_review_endpoint = admin_instance_request(
     route(
-        GET=render_template("treemap/photo_review.html",
-                            photo_review)))
+        GET=render_template('treemap/photo_review.html')(
+            photo_review)))
 
-photo_review_partial_endpoint = admin_instance_request(
-    route(
-        GET=render_template("treemap/partials/photo_review.html",
-                            photo_review)))
+photo_review_partial_endpoint = do(
+    admin_instance_request,
+    route(GET=do(
+        render_template('treemap/partials/photo_review.html'),
+        photo_review)))
 
-next_photo_endpoint = admin_instance_request(
-    route(
-        GET=render_template("treemap/partials/photo.html",
-                            next_photo)))
+next_photo_endpoint = do(
+    admin_instance_request,
+    route(GET=do(
+        render_template('treemap/partials/photo.html'),
+        next_photo)))
 
 approve_or_reject_photo_view = admin_instance_request(
     route(POST=approve_or_reject_photo))
