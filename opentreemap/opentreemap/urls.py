be81715a97fefafcc37bfa6636d694c5b72a0822
Michael Maurizi <mmaurizi@azavea.com>
2014-06-13 11:26:02 -0400

    Add endpoint that returns GeoJSON for all public instances

--- opentreemap/opentreemap/urls.py
+++ opentreemap/opentreemap/urls.py
@@ -14,7 +14,8 @@ from treemap.views import (user_view, root_settings_js_view,
                            profile_to_user_view, user_audits_view,
                            instance_not_available_view, update_user_view,
                            unsupported_view, landing_view, scss_view,
-                           upload_user_photo_view, forgot_username_view)
+                           upload_user_photo_view, forgot_username_view,
+                           instances_geojson_view)
 from treemap.instance import URL_NAME_PATTERN
 from treemap.urls import USERNAME_PATTERN
 from treemap.ecobenefits import within_itree_regions_view
@@ -68,6 +69,7 @@ urlpatterns = patterns(
     url(r'^main\.css$', scss_view, name='scss'),
     url(r'^eco/benefit/within_itree_regions/$', within_itree_regions_view,
         name='within_itree_regions'),
+    url(r'^instances/$', instances_geojson_view),
     url(instance_pattern + r'/accounts/register/$',
         RegistrationView.as_view(),
        name='instance_registration_register'),
--- opentreemap/treemap/views/misc.py
+++ opentreemap/treemap/views/misc.py
@@ -14,7 +14,7 @@ from django.core.exceptions import ValidationError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404
 
-from treemap.models import User, Species, StaticPage, MapFeature
+from treemap.models import User, Species, StaticPage, MapFeature, Instance
 
 from treemap.views.user import get_audits, get_audits_params
 
@@ -174,3 +174,41 @@ def compile_scss(request):
     scss = scss.encode('utf-8')
 
     return sass.compile(string=scss, include_paths=[settings.SCSS_ROOT])
+
+
+def public_instances_geojson(request):
+    def instance_geojson(instance):
+        return {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [instance.center_lat_lng.x,
+                                instance.center_lat_lng.y]
+            },
+            'properties': {
+                'name': instance.name,
+                'url': reverse(
+                    'instance_index_view',
+                    kwargs={'instance_url_name': instance.url_name}),
+                'tree_count': instance.tree_count,
+                'plot_count': instance.plot_count
+            }
+        }
+
+    tree_query = "SELECT COUNT(*) FROM treemap_tree WHERE "\
+        "treemap_tree.instance_id = treemap_instance.id"
+
+    plot_query = "SELECT COUNT(*) FROM treemap_mapfeature"\
+        " WHERE treemap_mapfeature.instance_id = treemap_instance.id"\
+        " AND treemap_mapfeature.feature_type = 'Plot'"
+
+    # You might think you can do .annotate(tree_count=Count('tree'))
+    # But it is horribly slow due to too many group bys
+    instances = (Instance.objects
+                 .filter(is_public=True)
+                 .extra(select={
+                     'tree_count': tree_query,
+                     'plot_count': plot_query
+                 }))
+
+    return [instance_geojson(instance) for instance in instances]
