0231e71afccbb0d063a288357123f03895897aa5
Justin Walgran <jwalgran@azavea.com>
2014-09-10 17:50:25 -0400

    Change instance comment base endpoint
    I made endpoints for flag and unflag and these look most natural as
    
    POST /{instance}/comments/{comment_id}/flag/
    POST /{instance}/comments/{comment_id}/unflag/

--- opentreemap/opentreemap/urls.py
+++ opentreemap/opentreemap/urls.py
@@ -75,7 +75,7 @@ urlpatterns = patterns(
        name='instance_registration_register'),
     url(instance_pattern + r'/', include('treemap.urls')),
     url(instance_pattern + r'/export/', include('exporter.urls')),
-    url(instance_pattern + r'/comment-review/', include('otm_comments.urls'))
+    url(instance_pattern + r'/comments/', include('otm_comments.urls'))
 )
 
 if settings.USE_JS_I18N:
