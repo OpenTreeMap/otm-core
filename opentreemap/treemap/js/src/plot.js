OpenTreeMap/otm-core

commit 1f0f44e17f36206904f82ffc4fd589a345b05f67

Convert UDF collection values to YYYY-MM-DD when saving.
Fixes #1657

opentreemap/treemap/js/src/plot.js
@@ -24,7 +24,7 @@ exports.init = function(options) {
     var udfRowTemplate = _.template(
         '<tr data-value-id="">' +
             '<% _.each(fields, function (field) { %>' +
-            '<td> <%= field %> </td>' +
+            '<td data-value="<%= field.value %>"> <%= field.display %> </td>' +
             '<% }) %>' +
             '</tr>');
 
@@ -34,10 +34,20 @@ exports.init = function(options) {
         var fields = $('table[data-udf-id="' + id + '"] * [data-field-name]').toArray();
 
         var data = _.map(fields, function(field) {
-            if ($(field).attr('data-moment-date-format')) {
-                return moment($(field).datepicker("getDate")).format($(field).attr('data-moment-date-format'));
+            var $field = $(field);
+            if ($field.attr('data-date-display-format')) {
+                var displayFormat = $field.attr('data-date-display-format'),
+                    valueFormat = $field.attr('data-date-serialize-format'),
+                    date = $field.datepicker("getDate");
+                return {
+                    display: moment(date).format(displayFormat),
+                    value: moment(date).format(valueFormat),
+                };
             } else {
-                return $(field).val();
+                return {
+                    value: $field.val(),
+                    display: $field.val()
+                };
             }
         });
 

opentreemap/treemap/js/src/inlineEditForm.js
@@ -204,6 +204,7 @@ exports.init = function(options) {
                         .map(function() {
                             return $(this).html();
                         });
+
                 headers = _.compact(headers);
 
                 data[name] =
@@ -214,7 +215,7 @@ exports.init = function(options) {
 
                             rowData = _.object(headers, $tds
                                         .map(function() {
-                                            return $.trim($(this).html());
+                                            return $.trim($(this).attr('data-value'));
                                         }));
                         if (! _.isEmpty(id)) {
                             rowData.id = id;

opentreemap/treemap/templates/treemap/partials/collection_udf_row.html
@@ -6,15 +6,17 @@
    {% endif %}
    >
   {% for field in udf.datatype_dict %}
-  <td>
   {% with field_value=value|get:field.name %}
     {% if field.type == "date" %}
-    {{ field_value|date:request.instance.short_date_format }}
-    {% else %}
-    {{ field_value }}
+    <td data-value="{{ field_value|date:"Y-m-d" }}">
+      {{ field_value|date:request.instance.short_date_format }}
+    </td>
+      {% else %}
+    <td data-value="{{ field_value }}">
+      {{ field_value }}
+    </td>
     {% endif %}
   {% endwith %}
-  </td>
   {% endfor %}
   <td></td>
 </tr>

opentreemap/treemap/templates/treemap/partials/udf_edit_field.html
@@ -1,4 +1,6 @@
 {% load i18n %}
+{% load dateformat %}
+
 {% if field.type == 'choice' %}
 <select class="form-control" data-field-name="{{ field.name }}">
   {% for choice in field.choices %}
@@ -6,8 +8,9 @@
   {% endfor %}
 </select>
 {% elif field.type == 'date' %}
-<input class="form-control" type="text" data-field-name="{{ field.name }}" data-date-format="yyyy-mm-dd"
-       data-moment-date-format="YYYY-MM-DD" value="{% trans "Today" %}" />
+<input class="form-control" type="text" data-field-name="{{ field.name }}"
+       data-date-display-format="{{ request.instance.short_date_format|moment_format }}"
+       data-date-serialize-format="YYYY-MM-DD" value="{% trans "Today" %}" />
 {% else %}
 <input type="text" data-field-name="{{ field.name }}" />
 {% endif %}

opentreemap/treemap/templatetags/dateformat.py
@@ -23,3 +23,28 @@ def datepicker_format(date_format):
     date_format = date_format.replace('Y', 'yyyy')
 
     return date_format
+
+
+@register.filter
+@stringfilter
+def moment_format(date_format):
+    """
+    Converts a Django DATE_FORMAT to the one specified at:
+    http://momentjs.com/docs/#/displaying/
+    """
+    conv_dict = {
+        'j': 'D',
+        'd': 'DD',
+        'D': 'ddd',
+        'l': 'dddd',
+        'n': 'M',
+        'm': 'MM',
+        'N': 'MMM',
+        'M': 'MMM',
+        'F': 'MMMM',
+        'E': 'MMMM',  # Locale text month,
+        'y': 'YY',
+        'Y': 'YYYY'
+    }
+    moment_format = ''.join(conv_dict.get(ch, ch) for ch in date_format)
+    return moment_format

commit 2a37120f2906e99923143227f42d2e7156ff6865

Set data attribute when resolving alerts
Fixes #1698

This fixed a regression introduced by 1f0f44e1, which stopped reading
values from .html() and started reading values from .attr(). The resolve
button was setting the body text of the element, but not the data
attribute.

opentreemap/treemap/js/src/plot.js
@@ -123,6 +123,7 @@ exports.init = function(options) {
         $tables.find('.resolveBtn').click(function () {
             $(this).closest('tr')
                 .find('td:contains("Unresolved")')
+                .attr('data-value', 'Resolved')
                 .text('Resolved');
         });
     }