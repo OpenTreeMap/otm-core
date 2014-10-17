2ae6a05f260ec0227edfcd3b9eeccb991c6880e5
Justin Walgran <jwalgran@azavea.com>
2014-10-17 13:08:41 -0400

    Add unix timestamp helper
    The Stripe APIs use Unix timestamps. This helper wraps up the somewhat
    ugly method of creating one in Python.

--- opentreemap/treemap/lib/dates.py
+++ opentreemap/treemap/lib/dates.py
@@ -1,4 +1,6 @@
 from datetime import datetime
+from django.utils.timezone import now
+import calendar
 
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
 DATE_FORMAT = '%Y-%m-%d'
@@ -10,3 +12,11 @@ def parse_date_string_with_or_without_time(date_string):
     except ValueError:
         # If the time is not included, try again with date only
         return datetime.strptime(date_string.strip(), '%Y-%m-%d')
+
+
+def unix_timestamp(d=None):
+    if d is None:
+        d = now()
+        return calendar.timegm(d.utctimetuple())
+    else:
+        return calendar.timegm(d.timetuple())
