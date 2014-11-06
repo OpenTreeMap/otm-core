895f853a47cb026ded7e067be45e075acfc24663
Steve Lamb <slamb@azavea.com>
2014-11-05 20:34:47 -0500

    Add helper function for comparing foreign keys

--- opentreemap/opentreemap/util.py
+++ opentreemap/opentreemap/util.py
@@ -106,3 +106,30 @@ def dict_pop(dictionary, query):
         return (match, True)
     else:
         return (None, False)
+
+
+def force_obj_to_pk(obj):
+    """
+    A utility function for safely forcing a foreign-key/related object to
+    its primary key, for comparison purposes.
+
+    Django uses polymorphism in the ORM and model api to allow pks to stand
+    in for bonafide object, and vice versa. Consequently, it's easy to lose
+    track of what type the objects and scope are instances of. This becomes
+    a problem when *comparison* happens, Because comparing a primary key to
+    a model with that primary key does not return True, as expected.
+
+    Example:
+    foo = Tree.objects.all()[0]
+    bar = Tree.objects.all()[0].pk
+    assert(foo != bar)
+    assert(force_obj_to_pk(foo) == force_obj_to_pk(bar))
+    """
+    if obj is None:
+        return None
+    elif hasattr(obj, 'pk'):
+        return obj.pk
+    elif hasattr(obj, 'id'):
+        return obj.id
+    else:
+        return obj
