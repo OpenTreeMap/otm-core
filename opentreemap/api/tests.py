OpenTreeMap/otm-core

commit 0eb39f023928ff6293fc0656dcd5c17e798fd355

Refactor treemap app to use the routes pattern

opentreemap/api/views.py
@@ -19,7 +19,7 @@ from django_tinsel.utils import decorate as do
 from django_tinsel.decorators import route, json_api_call
 
 from treemap.models import Plot, Tree, User
-from treemap.views import species_list
+from treemap.routes import species_list
 from treemap.lib.map_feature import context_dict_for_plot
 from treemap.lib.tree import add_tree_photo_helper
 from treemap.lib.photo import context_dict_for_photo

opentreemap/api/user.py
@@ -14,7 +14,7 @@ from django.utils.translation import ugettext as trans
 
 from registration.models import RegistrationProfile
 
-from treemap.views import upload_user_photo
+from treemap.views.user import upload_user_photo
 from treemap.models import User
 
 

opentreemap/opentreemap/urls.py
@@ -8,14 +8,7 @@ from django.conf import settings
 from django.views.generic import RedirectView
 from django.contrib import admin
 
-from django_tinsel.decorators import route
-
-from treemap.views import (user_view, root_settings_js_view,
-                           profile_to_user_view, user_audits_view,
-                           instance_not_available_view, update_user_view,
-                           unsupported_view, landing_view, scss_view,
-                           upload_user_photo_view, forgot_username_view,
-                           instances_geojson_view)
+from treemap import routes
 from treemap.instance import URL_NAME_PATTERN
 from treemap.urls import USERNAME_PATTERN
 from treemap.ecobenefits import within_itree_regions_view
@@ -43,33 +36,33 @@ urlpatterns = patterns(
         url='/static/img/favicon.png', permanent=False)),
     url('^comments/', include('django.contrib.comments.urls')),
     url(r'^', include('geocode.urls')),
-    url(r'^$', landing_view),
-    url(r'^config/settings.js$', root_settings_js_view),
+    url(r'^$', routes.landing_page),
+    url(r'^config/settings.js$', routes.root_settings_js),
     url(r'^users/%s/$' % USERNAME_PATTERN,
-        route(GET=user_view, PUT=update_user_view), name='user'),
+        routes.user, name='user'),
     url(r'^users/%s/edits/$' % USERNAME_PATTERN,
-        user_audits_view, name='user_audits'),
+        routes.user_audits, name='user_audits'),
     url(r'^users/%s/photo/$' % USERNAME_PATTERN,
-        upload_user_photo_view, name='user_photo'),
+        routes.upload_user_photo, name='user_photo'),
     url(r'^api/v(?P<version>\d+)/', include('api.urls')),
     # The profile view is handled specially by redirecting to
     # the page of the currently logged in user
-    url(r'^accounts/profile/$', profile_to_user_view, name='profile'),
+    url(r'^accounts/profile/$', routes.profile_to_user_page, name='profile'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout',
         {'next_page': '/'}),
-    url(r'^accounts/forgot-username/$', forgot_username_view,
+    url(r'^accounts/forgot-username/$', routes.forgot_username,
         name='forgot_username'),
     url(r'^accounts/', include('registration_backend.urls')),
     # Create a redirect view for setting the session language preference
     # https://docs.djangoproject.com/en/1.0/topics/i18n/#the-set-language-redirect-view  # NOQA
     url(r'^i18n/', include('django.conf.urls.i18n')),
-    url(r'^not-available$', instance_not_available_view,
+    url(r'^not-available$', routes.instance_not_available,
         name='instance_not_available'),
-    url(r'^unsupported$', unsupported_view, name='unsupported'),
-    url(r'^main\.css$', scss_view, name='scss'),
+    url(r'^unsupported$', routes.unsupported_page, name='unsupported'),
+    url(r'^main\.css$', routes.compile_scss, name='scss'),
     url(r'^eco/benefit/within_itree_regions/$', within_itree_regions_view,
         name='within_itree_regions'),
-    url(r'^instances/$', instances_geojson_view),
+    url(r'^instances/$', routes.instances_geojson),
     url(instance_pattern + r'/accounts/register/$',
         RegistrationView.as_view(),
        name='instance_registration_register'),
@@ -86,9 +79,10 @@ if settings.USE_JS_I18N:
         'packages': settings.I18N_APPS,
     }
 
-    urlpatterns = patterns('', url(r'^jsi18n/$',
-                           'django.views.i18n.javascript_catalog',
-                           js_i18n_info_dict)) + urlpatterns
+    urlpatterns = patterns(
+        '', url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog',
+                js_i18n_info_dict)
+    ) + urlpatterns
 
 if settings.EXTRA_URLS:
     for (url_pattern, url_module) in settings.EXTRA_URLS:
@@ -99,7 +93,7 @@ if settings.DEBUG:
     urlpatterns = patterns(
         '', url(r'^admin/', include(admin.site.urls))) + urlpatterns
 
-handler404 = 'treemap.views.error_404_view'
-handler500 = 'treemap.views.error_500_view'
+handler404 = 'treemap.routes.error_404_page'
+handler500 = 'treemap.routes.error_500_page'
 # Not hooked up yet
-handler503 = 'treemap.views.error_503_view'
+handler503 = 'treemap.routes.error_503_page'

opentreemap/treemap/routes.py
@@ -1,0 +1,252 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import division
+
+from functools import partial
+
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import etag
+
+from django_tinsel.utils import decorate as do
+from django_tinsel.decorators import (route, json_api_call, render_template,
+                                      string_to_response,
+                                      username_matches_request_user)
+
+from treemap.decorators import (login_or_401, return_400_if_validation_errors,
+                                require_http_method, requires_feature,
+                                creates_instance_user, instance_request,
+                                admin_instance_request, json_api_edit)
+
+import treemap.views.user as user_views
+import treemap.views.photo as photo_views
+import treemap.views.tree as tree_views
+import treemap.views.misc as misc_views
+import treemap.views.map_feature as feature_views
+
+
+add_map_feature_photo_do = partial(
+    do,
+    require_http_method("POST"),
+    login_or_401,
+    instance_request,
+    creates_instance_user,
+    render_template('treemap/partials/photo_carousel.html'))
+
+#####################################
+# misc content views
+#####################################
+
+edits_page = do(
+    instance_request,
+    requires_feature('recent_edits_report'),
+    render_template('treemap/edits.html'),
+    misc_views.edits)
+
+index_page = instance_request(misc_views.index)
+
+map_page = do(
+    instance_request,
+    render_template('treemap/map.html'),
+    misc_views.get_map_view_context)
+
+static_page = do(
+    instance_request,
+    render_template('treemap/staticpage.html'),
+    misc_views.static_page)
+
+instance_not_available = render_template(
+    'treemap/instance_not_available.html')()
+
+landing_page = render_template('base.html')()
+
+unsupported_page = render_template('treemap/unsupported.html')()
+
+instances_geojson = do(
+    json_api_call,
+    misc_views.public_instances_geojson)
+
+error_404_page = render_template('404.html')(statuscode=404)
+error_500_page = render_template('500.html')(statuscode=500)
+error_503_page = render_template('503.html')(statuscode=503)
+
+#####################################
+# utility views
+#####################################
+
+root_settings_js = render_template('treemap/settings.js')(
+    {'BING_API_KEY':
+     settings.BING_API_KEY},
+    mimetype='application/javascript')
+
+instance_settings_js = instance_request(root_settings_js)
+
+boundary_to_geojson = do(
+    json_api_call,
+    instance_request,
+    misc_views.boundary_to_geojson)
+
+boundary_autocomplete = do(
+    json_api_call,
+    instance_request,
+    misc_views.boundary_autocomplete)
+
+species_list = do(
+    json_api_call,
+    instance_request,
+    misc_views.species_list)
+
+compile_scss = do(
+    require_http_method("GET"),
+    string_to_response("text/css"),
+    misc_views.compile_scss)
+
+#####################################
+# mapfeature
+#####################################
+
+add_map_feature = do(
+    instance_request,
+    route(
+        GET=feature_views.render_map_feature_add,
+        POST=do(
+            json_api_edit,
+            return_400_if_validation_errors,
+            feature_views.add_map_feature)))
+
+map_feature_detail = do(
+    instance_request,
+    route(
+        GET=feature_views.render_map_feature_detail,
+        ELSE=do(
+            json_api_edit,
+            return_400_if_validation_errors,
+            route(
+                PUT=feature_views.update_map_feature_detail,
+                DELETE=feature_views.delete_map_feature))))
+
+map_feature_accordion = do(
+    instance_request,
+    render_template('treemap/partials/map_feature_accordion.html'),
+    feature_views.map_feature_detail)
+
+get_map_feature_sidebar = do(
+    instance_request,
+    etag(feature_views.map_feature_hash),
+    render_template('treemap/partials/sidebar.html'),
+    feature_views.map_feature_detail)
+
+map_feature_popup = do(
+    instance_request,
+    etag(feature_views.map_feature_hash),
+    render_template('treemap/partials/map_feature_popup.html'),
+    feature_views.map_feature_popup)
+
+add_map_feature_photo = add_map_feature_photo_do(
+    feature_views.add_map_feature_photo)
+
+rotate_map_feature_photo = add_map_feature_photo_do(
+    feature_views.rotate_map_feature_photo)
+
+
+#####################################
+# plot
+#####################################
+
+edit_plot_detail = do(
+    login_required,
+    instance_request,
+    creates_instance_user,
+    render_template('treemap/plot_detail.html'),
+    feature_views.plot_detail)
+
+get_plot_eco = do(
+    instance_request,
+    etag(feature_views.map_feature_hash),
+    render_template('treemap/partials/plot_eco.html'),
+    feature_views.plot_detail)
+
+
+#####################################
+# tree
+#####################################
+
+delete_tree = do(
+    instance_request,
+    json_api_edit,
+    route(DELETE=tree_views.delete_tree))
+
+tree_detail = instance_request(tree_views.tree_detail)
+
+search_tree_benefits = do(
+    instance_request,
+    etag(tree_views.search_hash),
+    render_template('treemap/partials/eco_benefits.html'),
+    tree_views.search_tree_benefits)
+
+add_tree_photo = add_map_feature_photo_do(tree_views.add_tree_photo)
+
+#####################################
+# user
+#####################################
+
+instance_user_page = user_views.instance_user
+
+instance_user_audits = user_views.instance_user_audits
+
+profile_to_user_page = user_views.profile_to_user
+
+user = route(
+    GET=render_template('treemap/user.html')(user_views.user),
+    PUT=do(
+        require_http_method("PUT"),
+        username_matches_request_user,
+        json_api_call,
+        return_400_if_validation_errors,
+        user_views.update_user))
+
+user_audits = do(
+    render_template('treemap/recent_user_edits.html'),
+    user_views.user_audits)
+
+upload_user_photo = do(
+    require_http_method("POST"),
+    username_matches_request_user,
+    json_api_call,
+    return_400_if_validation_errors,
+    user_views.upload_user_photo)
+
+forgot_username = route(
+    GET=render_template('treemap/forgot_username.html')(),
+    POST=do(
+        render_template('treemap/forgot_username_done.html'),
+        return_400_if_validation_errors,
+        user_views.forgot_username))
+
+#####################################
+# photo
+#####################################
+
+photo_review = do(
+    require_http_method("GET"),
+    admin_instance_request,
+    render_template('treemap/photo_review.html'),
+    photo_views.photo_review)
+
+photo_review_partial = do(
+    require_http_method("GET"),
+    admin_instance_request,
+    render_template('treemap/partials/photo_review.html'),
+    photo_views.photo_review)
+
+next_photo = do(
+    require_http_method("GET"),
+    admin_instance_request,
+    render_template('treemap/partials/photo.html'),
+    photo_views.next_photo)
+
+approve_or_reject_photo = do(
+    require_http_method("POST"),
+    admin_instance_request,
+    photo_views.approve_or_reject_photo)

opentreemap/treemap/tests/test_ecobenefits.py
@@ -16,7 +16,7 @@ from treemap.ecobenefits import (TreeBenefitsCalculator,
                                  _combine_grouped_benefits)
 
 from treemap.ecobenefits import BenefitCategory
-from treemap.views import search_tree_benefits
+from treemap.views.tree import search_tree_benefits
 
 
 class EcoTest(UrlTestCase):

opentreemap/treemap/tests/test_views.py
@@ -28,18 +28,19 @@ from treemap.audit import (Audit, approve_or_reject_audit_and_apply,
                            FieldPermission, add_default_permissions)
 from treemap.models import (Instance, Species, User, Plot, Tree, TreePhoto,
                             InstanceUser, StaticPage, ITreeRegion)
-from treemap.views import (species_list, boundary_to_geojson, plot_detail,
-                           boundary_autocomplete, edits, user_audits,
-                           root_settings_js_view, instance_settings_js_view,
-                           compile_scss, approve_or_reject_photo,
-                           upload_user_photo, static_page, instance_user_view,
-                           delete_map_feature, delete_tree, user,
-                           forgot_username, update_user)
+from treemap.routes import (root_settings_js, instance_settings_js,
+                            instance_user_page)
 from treemap.lib.tree import add_tree_photo_helper
 from treemap.lib.user import get_user_instances
-from treemap.views.misc import public_instances_geojson
-from treemap.views.map_feature import (update_map_feature,
-                                       rotate_map_feature_photo)
+from treemap.views.misc import (public_instances_geojson, species_list,
+                                boundary_autocomplete, boundary_to_geojson,
+                                edits, compile_scss, static_page)
+from treemap.views.map_feature import (update_map_feature, delete_map_feature,
+                                       rotate_map_feature_photo, plot_detail)
+from treemap.views.user import (user_audits, upload_user_photo, update_user,
+                                forgot_username, user)
+from treemap.views.photo import approve_or_reject_photo
+from treemap.views.tree import delete_tree
 from treemap.tests import (ViewTestCase, make_instance, make_officer_user,
                            make_commander_user, make_apprentice_user,
                            make_simple_boundary, make_request, make_user,
@@ -1624,7 +1625,7 @@ class InstanceUserViewTests(ViewTestCase):
         self.commander = make_user(username="commander", password='pw')
 
     def test_get_by_username_redirects(self):
-        res = instance_user_view(make_request(),
+        res = instance_user_page(make_request(),
                                  self.instance.url_name,
                                  self.commander.username)
         expected_url = '/users/%s/?instance_id=%d' %\
@@ -1637,7 +1638,7 @@ class InstanceUserViewTests(ViewTestCase):
 
     def test_get_with_invalid_username_redirects(self):
         test_username = 'no_way_username'
-        res = instance_user_view(make_request(),
+        res = instance_user_page(make_request(),
                                  self.instance.url_name,
                                  test_username)
         expected_url = '/users/%s/?instance_id=%d' %\
@@ -1666,7 +1667,7 @@ class SettingsJsViewTests(ViewTestCase):
         self.user = make_commander_user(self.instance)
         self.req = make_request(user=self.user)
         self.req.session = MockSession()
-        self.get_response = lambda: root_settings_js_view(self.req)
+        self.get_response = lambda: root_settings_js(self.req)
 
     @override_settings(TILE_HOST=None)
     def test_none_tile_hosts_omits_tilehosts_setting(self):
@@ -1688,7 +1689,7 @@ class InstanceSettingsJsViewTests(SettingsJsViewTests):
     """
     def setUp(self):
         super(InstanceSettingsJsViewTests, self).setUp()
-        self.get_response = lambda: instance_settings_js_view(
+        self.get_response = lambda: instance_settings_js(
             self.req, self.instance.url_name)
 
 

opentreemap/treemap/urls.py
@@ -5,23 +5,7 @@ from __future__ import division
 
 from django.conf.urls import patterns, url
 
-from django_tinsel.decorators import route
-
-from treemap.views import (boundary_to_geojson_view, index_view, map_view,
-                           delete_tree_view, instance_settings_js_view,
-                           edits_view,
-                           search_tree_benefits_view, species_list_view,
-                           boundary_autocomplete_view, instance_user_view,
-                           map_feature_popup_view, instance_user_audits_view,
-                           map_feature_accordion_view,
-                           add_tree_photo_endpoint, photo_review_endpoint,
-                           photo_review_partial_endpoint,
-                           approve_or_reject_photo_view, next_photo_endpoint,
-                           get_plot_eco_view, edit_plot_detail_view,
-                           static_page_view, get_map_feature_sidebar_view,
-                           tree_detail_view, add_map_feature_photo_endpoint,
-                           map_feature_detail_view, map_feature_add_view,
-                           rotate_map_feature_photo_endpoint)
+from treemap import routes
 
 # Testing notes:
 # We want to test that every URL succeeds (200) or fails with bad data (404).
@@ -32,49 +16,49 @@ USERNAME_PATTERN = r'(?P<username>[\w.@+-]+)'
 
 urlpatterns = patterns(
     '',
-    url(r'^$', index_view, name='instance_index_view'),
+    url(r'^$', routes.index_page, name='instance_index_view'),
     url(r'page/(?P<page>[a-zA-Z0-9 ]+)/$',
-        static_page_view, name='static_page'),
+        routes.static_page, name='static_page'),
     url(r'^boundaries/(?P<boundary_id>\d+)/geojson/$',
-        boundary_to_geojson_view),
-    url(r'^boundaries/$', boundary_autocomplete_view),
-    url(r'^edits/$', edits_view, name='edits'),
-    url(r'^photo_review_full/$', photo_review_endpoint),
-    url(r'^photo_review/$', photo_review_partial_endpoint,
+        routes.boundary_to_geojson),
+    url(r'^boundaries/$', routes.boundary_autocomplete),
+    url(r'^edits/$', routes.edits_page, name='edits'),
+    url(r'^photo_review_full/$', routes.photo_review),
+    url(r'^photo_review/$', routes.photo_review_partial,
         name='photo_review'),
-    url(r'^photo_review/next$', next_photo_endpoint, name='photo_review_next'),
+    url(r'^photo_review/next$', routes.next_photo, name='photo_review_next'),
     url('^features/(?P<feature_id>\d+)/photo/(?P<photo_id>\d+)/'
         '(?P<action>(approve)|(reject))$',
-        approve_or_reject_photo_view, name='approve_or_reject_photo'),
-    url(r'^species/$', species_list_view, name="species_list_view"),
-    url(r'^map/$', map_view, name='map'),
+        routes.approve_or_reject_photo, name='approve_or_reject_photo'),
+    url(r'^species/$', routes.species_list, name="species_list_view"),
+    url(r'^map/$', routes.map_page, name='map'),
 
     url(r'^features/(?P<feature_id>\d+)/$',
-        map_feature_detail_view, name='map_feature_detail'),
+        routes.map_feature_detail, name='map_feature_detail'),
     url(r'^features/(?P<type>\w+)/$',
-        map_feature_add_view, name='add_map_feature'),
+        routes.add_map_feature, name='add_map_feature'),
     url(r'^features/(?P<feature_id>\d+)/(?P<edit>edit)$',
-        edit_plot_detail_view, name='map_feature_detail_edit'),
+        routes.edit_plot_detail, name='map_feature_detail_edit'),
     url(r'^features/(?P<feature_id>\d+)/popup$',
-        map_feature_popup_view, name='map_feature_popup'),
+        routes.map_feature_popup, name='map_feature_popup'),
     url(r'^features/(?P<feature_id>\d+)/trees/(?P<tree_id>\d+)/$',
-        route(DELETE=delete_tree_view)),
+        routes.delete_tree),
     url(r'^features/(?P<feature_id>\d+)/sidebar$',
-        get_map_feature_sidebar_view, name='map_feature_sidebar'),
+        routes.get_map_feature_sidebar, name='map_feature_sidebar'),
     url(r'^features/(?P<feature_id>\d+)/photo$',
-        add_map_feature_photo_endpoint, name='add_photo_to_map_feature'),
+        routes.add_map_feature_photo, name='add_photo_to_map_feature'),
     url(r'^features/(?P<feature_id>\d+)/detail$',
-        map_feature_accordion_view, name='map_feature_accordion'),
+        routes.map_feature_accordion, name='map_feature_accordion'),
     url('^features/(?P<feature_id>\d+)/photo/(?P<photo_id>\d+)$',
-        rotate_map_feature_photo_endpoint, name='rotate_photo'),
+        routes.rotate_map_feature_photo, name='rotate_photo'),
 
-    url(r'^plots/$', route(POST=map_feature_add_view), name='add_plot'),
+    url(r'^plots/$', routes.add_map_feature, name='add_plot'),
     url(r'^plots/(?P<feature_id>\d+)/eco$',
-        get_plot_eco_view, name='plot_eco'),
+        routes.get_plot_eco, name='plot_eco'),
     url(r'^plots/(?P<feature_id>\d+)/trees/(?P<tree_id>\d+)/eco$',
-        get_plot_eco_view, name='tree_eco'),
+        routes.get_plot_eco, name='tree_eco'),
     url(r'^plots/(?P<feature_id>\d+)/trees/(?P<tree_id>\d+)/$',
-        tree_detail_view, name='tree_detail'),
+        routes.tree_detail, name='tree_detail'),
 
     # TODO: this duplication exists in multiple places.
     # we make two endpoints for 'plots/<id>/tree/<id>/' and 'plots/<id>/'
@@ -84,14 +68,14 @@ urlpatterns = patterns(
     # way to handle this duplication and reduce the total number of endpoints
     # for great good.
     url(r'^plots/(?P<feature_id>\d+)/photo$',
-        add_tree_photo_endpoint, name='add_photo_to_plot'),
+        routes.add_tree_photo, name='add_photo_to_plot'),
     url(r'^plots/(?P<feature_id>\d+)/tree/(?P<tree_id>\d+)/photo$',
-        add_tree_photo_endpoint, name='add_photo_to_tree'),
+        routes.add_tree_photo, name='add_photo_to_tree'),
 
     url(r'^config/settings.js$',
-        instance_settings_js_view, name='settings'),
-    url(r'^benefit/search$', search_tree_benefits_view),
-    url(r'^users/%s/$' % USERNAME_PATTERN, instance_user_view,
+        routes.instance_settings_js, name='settings'),
+    url(r'^benefit/search$', routes.search_tree_benefits),
+    url(r'^users/%s/$' % USERNAME_PATTERN, routes.instance_user_page,
         name="user_profile"),
-    url(r'^users/%s/edits/$' % USERNAME_PATTERN, instance_user_audits_view),
+    url(r'^users/%s/edits/$' % USERNAME_PATTERN, routes.instance_user_audits),
 )

commit c3055d28b00582bd4c2b8704979927666912243a

Fix species list api double decoration error
The species list API endpoint was failing with a strange "Can't adapt
type 'Instance'" error. It turns out this was caused by a refactor in
0eb39f023 which resulted in the species list endpoint being wrapped with
the ``instance_request`` decorator twice.

opentreemap/api/tests.py
@@ -1773,3 +1773,17 @@ class PasswordResetTest(OTMTestCase):
         response = post_json(url % (API_PFX, self.jim.email),
                              {}, self.client, None)
         self.assertEquals(response.status_code, 200)
+
+
+class SpeciesListTest(OTMTestCase):
+    def setUp(self):
+        self.instance = setupTreemapEnv()
+        self.user = User.objects.get(username="jim")
+
+    def test_species_list_endpoint(self):
+        response = get_signed(
+            self.client,
+            "%s/instance/%s/species" % (API_PFX,
+                                        self.instance.url_name),
+            user=self.user)
+        self.assertEqual(response.status_code, 200)

opentreemap/api/views.py
@@ -19,7 +19,7 @@ from django_tinsel.utils import decorate as do
 from django_tinsel.decorators import route, json_api_call
 
 from treemap.models import Plot, Tree, User
-from treemap.routes import species_list
+from treemap.views.misc import species_list
 from treemap.lib.map_feature import context_dict_for_plot
 from treemap.lib.tree import add_tree_photo_helper
 from treemap.lib.photo import context_dict_for_photo