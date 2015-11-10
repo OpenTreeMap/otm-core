OpenTreeMap/otm-core

commit 07c776c08f4c2109d9b1da769a39fae636a8fa89

Improve helpers
* `buttonEnabler.js` no longer requires `options.config` argument
* Add JS Utility `modalsFocusOnFirstInputWhenShown`
* Allow adding buttons to `form_buttons.html` partial

opentreemap/treemap/js/src/buttonEnabler.js
@@ -67,9 +67,8 @@ function fullyDisable($el, disabledTitle) {
 }
 
 exports.run = function (options) {
-    var $elements = $(enablePermSelector);
-
-    config = options.config;
+    var $elements = $(enablePermSelector),
+        loggedIn = options && options.config ? options.config.loggedIn : true;
 
     _.each($elements, function(element) {
         var $element = $(element),
@@ -79,7 +78,7 @@ exports.run = function (options) {
 
         if (hasPerm === 'True') {
             fullyEnable($element, href);
-        } else if (!config.loggedIn) {
+        } else if (!loggedIn) {
             makeRedirectToLogin($element, href);
         } else {
             fullyDisable($element, disabledTitle);

opentreemap/treemap/js/src/utility.js
@@ -200,3 +200,9 @@ exports.exportToCsv = function (rows, filename) {
         }
     }
 };
+
+exports.modalsFocusOnFirstInputWhenShown = function () {
+    $('.modal').on('shown.bs.modal', function() {
+        $(this).find('input').first().focus().select();
+    });
+};

opentreemap/treemap/templates/treemap/partials/form_buttons.html
@@ -2,6 +2,8 @@
 
 <div class="col-xs-4 text-right">
     <img class="spinner" src="{{ STATIC_URL }}img/spinner.gif" style="display: none;">
+    {% block other_buttons %}
+    {% endblock other_buttons %}
     <button data-class="display" class="btn btn-info editBtn">
         {% trans "Edit" %}
     </button>

commit 4c68196e7ad0553ebddb29fd658f4b6e2b428120

Require config argument to button enabler once again
07c776c08f4c2109d9b1da769a39fae636a8fa89 introduced an error by making the `config` argument
to `buttonEnabler` optional and eliminating the top-level `config` variable. This reverts
the first change, but retains the second so the usage of `config` is clearer.

opentreemap/treemap/js/src/buttonEnabler.js
@@ -33,9 +33,9 @@ function removeActionableDataAttributes($el) {
     $el.removeAttr('data-action');
 }
 
-function makeRedirectToLogin($el, href) {
+function makeRedirectToLogin(loginUrl, $el, href) {
 
-    var fullHref = config.loginUrl + href;
+    var fullHref = loginUrl + href;
 
     $el.attr('disabled', false);
     removeActionableDataAttributes($el);
@@ -68,7 +68,7 @@ function fullyDisable($el, disabledTitle) {
 
 exports.run = function (options) {
     var $elements = $(enablePermSelector),
-        loggedIn = options && options.config ? options.config.loggedIn : true;
+        config = options.config;
 
     _.each($elements, function(element) {
         var $element = $(element),
@@ -78,8 +78,8 @@ exports.run = function (options) {
 
         if (hasPerm === 'True') {
             fullyEnable($element, href);
-        } else if (!loggedIn) {
-            makeRedirectToLogin($element, href);
+        } else if (!config.loggedIn) {
+            makeRedirectToLogin(config.loginUrl, $element, href);
         } else {
             fullyDisable($element, disabledTitle);
         }