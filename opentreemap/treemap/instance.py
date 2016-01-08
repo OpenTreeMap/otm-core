e81a3a9bf0dd7ce5db34ea93eb208b4d51a1f48a
Steve Lamb <slamb@azavea.com>
2016-01-08 18:51:57 -0500

    Update search/mobile configs on gsi/udf db actions
    When doing any of the following:
    * remove a gsi model
    * remove the gsi module
    * delete a udf
    
    some/all of the following must be pruned:
    * mobile api fields
    * available search fields
    * missing data search fields

--- opentreemap/treemap/instance.py
+++ opentreemap/treemap/instance.py
@@ -372,6 +372,19 @@ class Instance(models.Model):
                                in self.map_feature_types
                                if class_name not in remove]
 
+        for class_name in self.map_feature_types:
+            if class_name not in remaining_types:
+                if class_name in self.search_config:
+                    del self.search_config[class_name]
+                if 'missing' in self.search_config:
+                    self.search_config['missing'] = [
+                        o for o in self.search_config['missing']
+                        if not o.get('identifier', '').startswith(
+                            to_object_name(class_name))]
+                # TODO: delete from mobile_api_fields
+                # non-plot mobile_api_fields are not currently
+                # supported, but when they are added, they should
+                # also be removed here.
         self.map_feature_types = remaining_types
         self.save()
 
