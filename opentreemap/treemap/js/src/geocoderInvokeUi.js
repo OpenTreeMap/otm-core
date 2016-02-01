872f4b5f5eb348f5a7284e7f9588eb1cdf6b05d1
RickMohr <rmohr@azavea.com>
2016-02-01 15:14:13 -0500

    Front end handles geocode search with and without an autocomplete suggestion
    Distinguish 3 cases when user initiates a search:
    
    1. User picked a geocoder suggestion. Call geocode endpoint, passing magic key and address text.
    2. User picked a non-geocoder choice (e.g. an OTM boundary). Don't call geocode endpoint.
    3. User didn't pick an autocomplete suggestion, or did but then edited it. Call geocode endpoint, passing just address text.
    
    Also, when calling geocoder pass instance bounding box so it can be used for the non-magic-key case.

--- opentreemap/treemap/js/src/geocoderInvokeUi.js
+++ opentreemap/treemap/js/src/geocoderInvokeUi.js
@@ -1,18 +1,37 @@
 "use strict";
 
 var $ = require('jquery'),
-    Bacon = require('baconjs'),
-    BU = require('treemap/baconUtils'),
+    _ = require('lodash'),
     geocoder = require('treemap/geocoder'),
     otmTypeahead = require('treemap/otmTypeahead');
 
 module.exports = function (options) {
     var $addressInput = $(options.addressInput),
-        geocodeCandidateStream = options.searchTriggerStream.map(function() {
-            return otmTypeahead.getDatum($addressInput);
-        }).filter('.magicKey'),
+        geocodeCandidateStream = options.searchTriggerStream
+            .map(getDatum)
+            .filter(_.identity),  // ignore false values
         gcoder = geocoder(options.config),
         geocodeResponseStream = gcoder.geocodeStream(geocodeCandidateStream);
 
     return geocodeResponseStream;
+
+    function getDatum() {
+        var datum = otmTypeahead.getDatum($addressInput),
+            inputText = $addressInput.val();
+        if (datum) {
+            if (datum.text) {
+                if (datum.text.startsWith(inputText)) {
+                    // A suggested address was chosen, so use it (with its magic key)
+                    return datum;
+                }
+            } else {
+                // Datum is not an address for searching (it's e.g. a boundary)
+                return false;
+            }
+        }
+        if (inputText) {
+            // Suggestion was not chosen or is stale, so use what the user typed
+            return {text: inputText};
+        }
+    }
 };
