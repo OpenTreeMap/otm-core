33f5b7cda9857756c193fe41010502ac22c07c0a
RickMohr <rmohr@azavea.com>
2016-02-01 15:14:13 -0500

    Front end handles geocode search with and without an autocomplete suggestion
    Distinguish 3 cases when user initiates a search:
    
    1. User picked a geocoder suggestion. Call geocode endpoint, passing magic key and address text.
    2. User picked a non-geocoder choice (e.g. an OTM boundary). Don't call geocode endpoint.
    3. User didn't pick an autocomplete suggestion, or did but then edited it. Call geocode endpoint, passing just address text.
    
    Also, when calling geocoder pass instance bounding box so it can be used for the non-magic-key case.

--- opentreemap/treemap/js/src/geocoderInvokeUi.js
+++ opentreemap/treemap/js/src/geocoderInvokeUi.js
@@ -1,18 +1,47 @@
 "use strict";
 
 var $ = require('jquery'),
-    Bacon = require('baconjs'),
-    BU = require('treemap/baconUtils'),
+    _ = require('lodash'),
     geocoder = require('treemap/geocoder'),
     otmTypeahead = require('treemap/otmTypeahead');
 
 module.exports = function (options) {
     var $addressInput = $(options.addressInput),
-        geocodeCandidateStream = options.searchTriggerStream.map(function() {
-            return otmTypeahead.getDatum($addressInput);
-        }).filter('.magicKey'),
+        geocodeCandidateStream = options.searchTriggerStream
+            .map(getDatum)
+            .filter(_.identity),  // ignore false values
         gcoder = geocoder(options.config),
         geocodeResponseStream = gcoder.geocodeStream(geocodeCandidateStream);
 
     return geocodeResponseStream;
+
+    function getDatum() {
+        var datum = otmTypeahead.getDatum($addressInput),
+            inputText = $addressInput.val();
+        if (datum) {
+            if (datum.magicKey) {
+                // Datum is an address suggestion
+                if (datum.text.startsWith(inputText)) {
+                    // Geocode this suggestion (using its magic key)
+                    return datum;
+                }
+                // else user has edited the suggestion; fall through
+            } else {
+                // Datum is a boundary
+                if (datum.value === inputText) {
+                    // We don't geocode boundaries
+                    return false;
+                }
+                // else user has edited the value; fall through
+            }
+        }
+        if (inputText) {
+            // The user either didn't choose a suggestion, or else chose
+            // a selection and then edited it. Geocode what the user typed.
+            return {text: inputText};
+        } else {
+            // No input text; don't geocode.
+            return false;
+        }
+    }
 };
