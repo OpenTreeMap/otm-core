78f4f26fb3eeec04d67cdd3e68e11dc58d8b672f
RickMohr <rmohr@azavea.com>
2016-03-10 11:38:26 -0500

    Do export stewardship

--- opentreemap/exporter/tasks.py
+++ opentreemap/exporter/tasks.py
@@ -211,8 +211,8 @@ def _csv_field_header_map(field_names):
     map = OrderedDict()
     # TODO: make this conditional based on whether or not
     # we are performing a complete export or an "importable" export
-    omit = {'readonly', 'udf:Stewardship',
-            'tree__readonly', 'tree__udf:Stewardship',
+    omit = {'readonly',
+            'tree__readonly',
             'tree__species', 'tree__id',
             'tree__species__fact_sheet_url',
             'tree__species__fall_conspicuous',
