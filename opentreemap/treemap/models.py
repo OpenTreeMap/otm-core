81178626dc0a2e9797613510e9de6acd661dc746
Justin Walgran <jwalgran@azavea.com>
2016-05-20 15:04:55 -0700

    Make Boundary.updated_at not nullable
    Removes a TODO from the code. When this field was added it was marked as
    nullable so that the migration would be backwards compatible. The
    the current production database does not have any null values in this
    column.

--- opentreemap/treemap/models.py
+++ opentreemap/treemap/models.py
@@ -1226,8 +1226,8 @@ class Boundary(models.Model):
     category = models.CharField(max_length=255)
     sort_order = models.IntegerField()
 
-    updated_at = models.DateTimeField(  # TODO: remove null=True
-        null=True, auto_now=True, editable=False, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True, editable=False,
+                                      db_index=True)
 
     objects = models.GeoManager()
 
