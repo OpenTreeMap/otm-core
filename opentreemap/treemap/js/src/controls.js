d3df641b4481c42b85403ddc67995d7844a77dc3
Kevin DeLoach <kdeloach@azavea.com>
2016-06-27 15:00:06 -0400

    Add regional canopy filter legend
    Connects #2629

--- opentreemap/treemap/js/src/controls.js
+++ opentreemap/treemap/js/src/controls.js
@@ -15,18 +15,17 @@ var CanopyFilterControl = L.Control.extend({
     },
 
     initialize: function() {
-        this._changeBus = new Bacon.Bus();
-        this.tilerArgsProp = this._changeBus.toProperty({
+        var self = this,
+            $el = $(canopyFilterTmpl()),
+            changeBus = new Bacon.Bus();
+
+        this.$el = $el;
+        this.tilerArgsProp = changeBus.toProperty({
             canopyMin: 0,
             canopyMax: 1
         });
-    },
 
-    onAdd: function(map) {
-        var self = this,
-            $el = $(canopyFilterTmpl());
-
-        $el.find('[name="canopy-slider"]').ionRangeSlider({
+        this.$el.find('[name="canopy-slider"]').ionRangeSlider({
             type: 'double',
             min: 0,
             max: 100,
