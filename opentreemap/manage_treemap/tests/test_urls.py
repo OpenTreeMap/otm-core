a3c4e4d9be49d14d15e6adfeda74a9aa7a9ab3b8
RickMohr <rmohr@azavea.com>
2017-04-10 16:03:19 -0400

    Move URL, route, view, and tests. Add URL test.
    Also move `admin_counts` endpoint.

--- opentreemap/manage_treemap/tests/test_urls.py
+++ opentreemap/manage_treemap/tests/test_urls.py
@@ -1,6 +1,6 @@
 from django.core.urlresolvers import reverse
 
-from treemap.tests import make_instance
+from treemap.tests import make_instance, make_admin_user
 from treemap.tests.test_urls import UrlTestCase
 
 
@@ -8,6 +8,8 @@ class PagesLoadTestCase(UrlTestCase):
 
     def setUp(self):
         self.instance = make_instance(is_public=True)
+        username = make_admin_user(self.instance).username
+        self.client.login(username=username, password='password')
 
     def assert_page_loads(self, django_url_name):
         self.assert_200(self.instance_reverse(django_url_name))
@@ -23,3 +25,6 @@ class PagesLoadTestCase(UrlTestCase):
     # TODO: test redirect once site_config page is moved
     # def test_management_redirects(self):
     #     self.assert_page_redirects('management', 'site_config')
+
+    def test_pages_load(self):
+        self.assert_page_loads('benefits')
