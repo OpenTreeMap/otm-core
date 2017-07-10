ce23775d3a76b0b8ecf349733454c0709cfe53d8
Michael Maurizi <mmaurizi@azavea.com>
2017-07-10 16:23:17 -0400

    Support Paginator.page_range returning an iterator or list
    Paginator.page_range will return an iterator in Django 1.9+

--- opentreemap/treemap/templatetags/paging.py
+++ opentreemap/treemap/templatetags/paging.py
@@ -6,10 +6,10 @@ register = template.Library()
 @register.filter
 def four_before_page(page_range, page):
     """Returns 4 or fewer pages before the given (1-based) page number"""
-    return page_range[max(page-5, 0):max(page-1, 0)]
+    return list(page_range)[max(page-5, 0):max(page-1, 0)]
 
 
 @register.filter
 def four_after_page(page_range, page):
     """Returns 4 or fewer pages after the given (1-based) page number"""
-    return page_range[page:min(page+4, len(page_range))]
+    return list(page_range)[page:min(page+4, len(page_range))]
