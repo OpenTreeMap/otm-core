b79a409a62d2c5675215f892c6183928e7a828df
Jenny Fung <fungjj92@gmail.com>
2020-01-23 13:01:12 -0500

    Encode image data in bytes not string
    Refs https://github.com/OpenTreeMap/otm-cloud/issues/505

--- opentreemap/treemap/tests/__init__.py
+++ opentreemap/treemap/tests/__init__.py
@@ -430,7 +430,7 @@ class LocalMediaTestCase(OTMTestCase):
         return path
 
     def load_resource(self, name):
-        return open(self.resource_path(name))
+        return open(self.resource_path(name), 'rb')
 
     def tearDown(self):
         shutil.rmtree(self.photoDir)
