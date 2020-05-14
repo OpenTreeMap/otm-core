4154dfb8a2176b9f5eef9f6af889f9155fd6caaa
Tom Z <tzinckgraf@gmail.com>
2020-05-14 11:43:28 -0400

    20200506_stewardship_removal - fixed emails

--- opentreemap/manage_treemap/lib/email.py
+++ opentreemap/manage_treemap/lib/email.py
@@ -9,19 +9,23 @@ def _get_email_subj_and_body(tmpl, ctxt):
     subject = ''.join(subject.splitlines())
 
     message = render_to_string(
+        'manage_treemap/emails/%s.plaintext.txt' % tmpl, ctxt)
+
+    html_message = render_to_string(
         'manage_treemap/emails/%s.body.txt' % tmpl, ctxt)
 
-    return subject, message
+    return subject, message, html_message
 
 
 def send_email(tmpl, ctxt, recipient_list):
     from_email = settings.DEFAULT_FROM_EMAIL
-    subject, message = _get_email_subj_and_body(tmpl, ctxt)
+    subject, message, html_message = _get_email_subj_and_body(tmpl, ctxt)
 
     return send_mail(
         subject,
         message,
-        'info@sustainablejc.org',
+        '"Sustainable JC Green Team" <info@sustainablejc.org>',
         recipient_list,
         fail_silently=False,
+        html_message=html_message
     )
